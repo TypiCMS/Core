5c4d5282810434e24fcbf1b2739f175d1eb3e199
Samuel De Backer <sdebacker@gmail.com>
2015-02-19 23:24:06 +0100

    cleaning base controllers

--- src/Http/Controllers/BaseAdminController.php
+++ src/Http/Controllers/BaseAdminController.php
@@ -1,54 +1,18 @@
 <?php
 namespace TypiCMS\Http\Controllers;
 
-use Config;
-use Illuminate\Foundation\Bus\DispatchesCommands;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Input;
-use Lang;
-use Patchwork\Utf8;
 use Response;
-use View;
 
 abstract class BaseAdminController extends BaseController
 {
 
     protected $repository;
 
-    // The cool kids’ way of handling page titles.
-    // https://gisglobal.github.com/jonathanmarvens/6017139
-    public $applicationName;
-    public $title  = array(
-        'parent'   => '',
-        'child'    => '',
-        'h1'       => '',
-    );
-
     public function __construct($repository = null)
     {
         $this->repository = $repository;
-
-        $this->applicationName = Config::get('typicms.' . Lang::getLocale() . '.website_title');
-
-        $instance = $this;
-        // View::composer($this->layout, function (\Illuminate\View\View $view) use ($instance) {
-        //     $view->with('title', $instance->getTitle());
-        // });
-
-        View::share('locales', Config::get('translatable.locales'));
-        View::share('locale', Config::get('app.locale'));
-    }
-
-    public function getTitle()
-    {
-        $title = Utf8::ucfirst($this->title['parent']);
-        if ($this->title['child']) {
-            $title .= ' – ' . Utf8::ucfirst($this->title['child']);
-        }
-        $title .= ' – ' . $this->applicationName;
-
-        return $title;
     }
 
     /**
--- src/Http/Controllers/BasePublicController.php
+++ src/Http/Controllers/BasePublicController.php
@@ -2,12 +2,8 @@
 namespace TypiCMS\Http\Controllers;
 
 use App;
-use Config;
-use Illuminate\Foundation\Bus\DispatchesCommands;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Input;
-use Patchwork\Utf8;
 use Sentry;
 use View;
 
@@ -16,31 +12,14 @@ abstract class BasePublicController extends BaseController
 
     protected $repository;
 
-    // The cool kids’ way of handling page titles.
-    // https://gist.github.com/jonathanmarvens/6017139
-    public $applicationName;
-    public $title  = array(
-        'parent'    => '',
-        'separator' => '',
-        'child'     => '',
-    );
-
     public function __construct($repository = null)
     {
         $this->repository = $repository;
-
-        $this->applicationName = Config::get('typicms.' . App::getLocale() . '.website_title');
-
-        $instance = $this;
-        // View::composer($this->layout, function (\Illuminate\View\View $view) use ($instance) {
-        //     $view->withTitle(Utf8::ucfirst(implode(' ', $instance->title)) . ' – ' . $instance->applicationName);
-        // });
 
         $bodyClass = ['lang-' . App::getLocale(), $repository->getModel()->getTable()];
         if (Sentry::getUser() && ! Input::get('preview')) {
             $bodyClass[] = 'has-navbar';
         }
-        View::share('lang', App::getLocale());
         View::share('bodyClass', implode(' ', $bodyClass));
     }
 }
