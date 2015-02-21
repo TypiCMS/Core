502963e69161e2e2f622a4634fe87b6980d8f394
Samuel De Backer <sdebacker@gmail.com>
2015-02-21 22:49:37 +0100

    controller

---
+++ src/Http/Controllers/BaseAdminController.php
@@ -0,0 +1,105 @@
+<?php
+namespace TypiCMS\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Input;
+use Response;
+
+abstract class BaseAdminController extends Controller
+{
+
+    protected $repository;
+
+    public function __construct($repository = null)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * List models
+     * 
+     * @return View
+     */
+    public function index()
+    {
+        $module = $this->repository->getTable();
+        $title = trans($module . '::global.name');
+        return view('core::admin.index')
+            ->with(compact('title', 'module'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return View
+     */
+    public function create()
+    {
+        $model = $this->repository->getModel();
+        return view('core::admin.create')
+            ->with(compact('model'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  $model
+     * @return void
+     */
+    public function edit($model)
+    {
+        return view('core::admin.edit')
+            ->with(compact('model'));
+    }
+
+    /**
+     * Show resource.
+     *
+     * @param  $model
+     * @return \Illuminate\Routing\Redirector
+     */
+    public function show($model)
+    {
+        return redirect($model->editUrl());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  $model
+     * @return \Illuminate\Routing\Redirector
+     */
+    public function destroy($model)
+    {
+        if ($this->repository->delete($model)) {
+            return back();
+        }
+    }
+
+    /**
+     * Sort list.
+     *
+     * @return Response
+     */
+    public function sort()
+    {
+        $this->repository->sort(Input::all());
+        return Response::json([
+            'error'   => false,
+            'message' => trans('global.Items sorted')
+        ], 200);
+    }
+
+    /**
+     * Redirect after a form is saved
+     * 
+     * @param  $request
+     * @param  $model
+     * @return \Illuminate\Routing\Redirector
+     */
+    protected function redirect($request, $model)
+    {
+        $redirectUrl = $request->get('exit') ? $model->indexUrl() : $model->editUrl() ;
+        return redirect($redirectUrl);
+    }
+}
