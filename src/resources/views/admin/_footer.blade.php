bbd830701fb90a00fb59a0b693552c2143c2d1a5
Samuel De Backer <sdebacker@gmail.com>
2015-03-17 14:05:26 +0100

    pickadate removed

--- src/resources/views/admin/_footer.blade.php
+++ src/resources/views/admin/_footer.blade.php
@@ -2,7 +2,6 @@
 
 @if(Config::get('typicms.admin_locale') != 'en')
     <script src="{{ asset('js/angular-locales/angular-locale_' . Config::get('typicms.admin_locale') . '-' . Config::get('typicms.admin_locale') . '.js') }}"></script>
-    <script src="{{ asset('js/pickadate-locales/' . Config::get('typicms.admin_locale') . '_' . strtoupper(Config::get('typicms.admin_locale')) . '.js') }}"></script>
 @endif
 
 @yield('js')
