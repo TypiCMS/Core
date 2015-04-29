c3ec7e1e1592b58e2d7c294af50366729c5035f1
Samuel De Backer <sdebacker@gmail.com>
2015-04-29 14:06:09 +0200

    unused method

--- src/Macros.php
+++ src/Macros.php
@@ -17,24 +17,3 @@ HTML::macro('langButton', function ($locale = null, $attributes = []) {
     return '<a ' . HTML::attributes($attributes) . '>' . $label . '</a>';
 
 });
-
-/**
- * Front office lang switcher
- */
-HTML::macro('languagesMenu', function (array $langsArray = array(), array $attributes = array()) {
-
-    $attributes['role'] = 'menu';
-    $attributes = HTML::attributes($attributes);
-
-    $html = array();
-    $html[] = '<ul ' . $attributes . '>';
-    foreach ($langsArray as $item) {
-        $html[] = '<li class="' . $item->class . '" role="menuitem">';
-        $html[] = '<a href="' . url($item->url) . '">' . $item->lang . '</a>';
-        $html[] = '</li>';
-    }
-    $html[] = '</ul>';
-
-    return implode("\r\n", $html);
-
-});
