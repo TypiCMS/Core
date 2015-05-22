dd07a5789dd4079d2935c9c5107070c329d10106
Samuel De Backer <sdebacker@gmail.com>
2015-05-22 23:10:42 +0200

    messages

--- src/resources/views/admin/_message.blade.php
+++ src/resources/views/admin/_message.blade.php
@@ -1,8 +1,8 @@
-@if (isset($status) or $status = $errors->first())
+@if ($message = Session::get('status') or $message = $errors->first())
     <div class="alert alert-info @if ($closable)alert-dismissable @endif">
         @if ($closable)
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
         @endif
-        <p>{{ $status }}</p>
+        <p>{{ $message }}</p>
     </div>
 @endif
