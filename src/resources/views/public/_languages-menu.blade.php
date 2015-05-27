3ae2b79ff7651e76ce365f9e9c14aebed1c1d09b
Samuel De Backer <sdebacker@gmail.com>
2015-05-27 14:23:04 +0200

    no language menu when only one locale is online

--- src/resources/views/public/_languages-menu.blade.php
+++ src/resources/views/public/_languages-menu.blade.php
@@ -1,6 +1,7 @@
+@if ($onlineLocales = TypiCMS::getPublicLocales() and count($onlineLocales) > 1)
 <nav class="nav-languages" role="navigation">
     <ul role="menu">
-        @foreach (TypiCMS::getPublicLocales() as $locale)
+        @foreach ($onlineLocales as $locale)
         <li class="@if($locale == config('app.locale'))active @endif" role="menuitem">
             @if (isset($model) and isset($page) and $model->hasTranslation())
             <a href="{{ url($page->uri($locale) . '/' . $model->translate($locale)->slug) }}">{{ $locale }}</a>
@@ -13,3 +14,4 @@
         @endforeach
     </ul>
 </nav>
+@endif
