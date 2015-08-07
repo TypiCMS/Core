342778a30c25d2c2414d9e178712fcf71044eee3
Samuel De Backer <sdebacker@gmail.com>
2015-08-07 14:42:36 +0200

    public link

--- src/resources/views/admin/_navbar-public-link.blade.php
+++ src/resources/views/admin/_navbar-public-link.blade.php
@@ -1,8 +1,8 @@
 @if (isset($model) and $model->id)
 <a href="{{ url($model->uri(config('app.locale'))) }}">
-@elseif (isset($module) and $page = TypiCMS::getPageLinkedToModule($module))
-<a href="{{ url($page->uri(config('app.locale'))) }}">
+@elseif (isset($module) and Route::has(config('app.locale') . '.' . $module))
+<a href="{{ route(config('app.locale') . '.' . $module) }}">
 @else
-<a href="/">
+<a href="{{ url('/') }}">
 @endif
 {{ ucfirst(trans('global.view website', [], null, config('typicms.admin_locale'))) }}</a>
