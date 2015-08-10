f4b820d82081dd5726c9bc28e6eae84e45ec3316
Samuel De Backer <sdebacker@gmail.com>
2015-08-10 20:18:36 +0200

    typekit_code removed

--- src/resources/views/public/master.blade.php
+++ src/resources/views/public/master.blade.php
@@ -21,11 +21,6 @@
 
     @yield('css')
 
-    @if(config('typicms.typekit_code'))
-    <script type="text/javascript" src="//use.typekit.net/{{ config('typicms.typekit_code') }}.js"></script>
-    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-    @endif
-
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -80,7 +75,7 @@
 
     </div>
 
-    @if(App::environment('production') and config('typicms.google_analytics_code'))
+    @if(env('production') and config('typicms.google_analytics_code'))
 
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
