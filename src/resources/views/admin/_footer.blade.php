8540b756edff78eb4059c39b3dac41121aea6b53
vizo <vizo@users.noreply.github.com>
2015-08-31 22:28:39 +0200

    Update _footer.blade.php

--- src/resources/views/admin/_footer.blade.php
+++ src/resources/views/admin/_footer.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ elixir('js/admin/components.min.js') }}"></script>
+<script src="{{ asset(elixir('js/admin/components.min.js')) }}"></script>
 
 @if(config('typicms.admin_locale') != 'en')
     <script src="{{ asset('js/angular-locales/angular-locale_' . config('typicms.admin_locale') . '-' . config('typicms.admin_locale') . '.js') }}"></script>
