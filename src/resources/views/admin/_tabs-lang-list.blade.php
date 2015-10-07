f7d329395dedb78c8e6eac885dccf5a59f8ff948
Samuel De Backer <sdebacker@gmail.com>
2015-10-07 15:10:34 +0200

    translatable.locale

--- src/resources/views/admin/_tabs-lang-list.blade.php
+++ src/resources/views/admin/_tabs-lang-list.blade.php
@@ -2,7 +2,7 @@
 <div class="btn-toolbar">
     <div class="btn-group pull-right">
     @foreach (config('translatable.locales') as $lang)
-        <a class="btn btn-default btn-xs @if ($lang == config('app.locale'))active @endif" href="?locale={{ $lang }}">@lang('global.languages.'.$lang)</a>
+        <a class="btn btn-default btn-xs @if ($lang == config('translatable.locale', config('app.locale')))active @endif" href="?locale={{ $lang }}">@lang('global.languages.'.$lang)</a>
     @endforeach
     </div>
 </div>
