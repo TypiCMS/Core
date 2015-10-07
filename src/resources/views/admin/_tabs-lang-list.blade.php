7768b9dbcfc30eacbdc92e3dbb5a34728bf08c69
Samuel De Backer <sdebacker@gmail.com>
2015-10-07 15:07:49 +0200

    locales btns

---
+++ src/resources/views/admin/_tabs-lang-list.blade.php
@@ -0,0 +1,9 @@
+@if (count(config('translatable.locales')) > 1)
+<div class="btn-toolbar">
+    <div class="btn-group pull-right">
+    @foreach (config('translatable.locales') as $lang)
+        <a class="btn btn-default btn-xs @if ($lang == config('app.locale'))active @endif" href="?locale={{ $lang }}">@lang('global.languages.'.$lang)</a>
+    @endforeach
+    </div>
+</div>
+@endif
