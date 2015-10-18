8bf1a0f4e88f5a1fcc9f3e32376f9c3db5716db1
Samuel De Backer <sdebacker@gmail.com>
2015-10-18 23:32:03 +0200

    translatable.locale

--- src/resources/views/admin/_tabs-lang-form.blade.php
+++ src/resources/views/admin/_tabs-lang-form.blade.php
@@ -2,7 +2,7 @@
 <div class="clearfix">
     <div class="btn-group pull-right" id="btn-group-form-locales">
         @foreach ($locales as $locale)
-            <a class="btn btn-default btn-xs @if($locale == config('app.locale'))active @endif" href="?{{ http_build_query(Input::except('locale') + ['locale' => $locale]) }}" data-target="#{{ $target }}-{{ $locale }}" data-toggle="tab">@lang('global.languages.'.$locale)</a>
+            <a class="btn btn-default btn-xs @if($locale == config('translatable.locale'))active @endif" href="?{{ http_build_query(Input::except('locale') + ['locale' => $locale]) }}" data-target="#{{ $target }}-{{ $locale }}" data-toggle="tab">@lang('global.languages.'.$locale)</a>
         @endforeach
     </div>
 </div>
