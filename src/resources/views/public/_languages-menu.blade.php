659146f35b1bcc8bde9a1d9938f6f9177695cb8e
Samuel De Backer <sdebacker@gmail.com>
2015-10-20 17:51:12 +0200

    languages menu in a dropdown

--- src/resources/views/public/_languages-menu.blade.php
+++ src/resources/views/public/_languages-menu.blade.php
@@ -1,6 +1,10 @@
 @if ($onlineLocales = TypiCMS::getOnlineLocales() and count($onlineLocales) > 1)
-<nav class="nav-languages">
-    <ul role="menu">
+<nav class="nav-languages dropdown">
+    <button class="btn dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+        {{ $lang }}
+        <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu" role="menu">
         @foreach ($onlineLocales as $locale)
         <li class="@if($locale == config('app.locale'))active @endif" role="menuitem">
             @if (isset($model) and isset($page) and $model->hasTranslation($locale))
