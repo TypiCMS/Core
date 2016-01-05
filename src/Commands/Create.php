64e8a5b44a8542f0e2f84aa8e03b7be7ac763b6c
Samuel De Backer <sdebacker@gmail.com>
2016-01-06 00:00:57 +0100

    Command to create a module

--- src/Commands/Create.php
+++ src/Commands/Create.php
@@ -18,6 +18,13 @@ class Create extends Command
     protected $files;
 
     /**
+     * The name of the module, pluralized and ucfirsted.
+     *
+     * @var string
+     */
+    protected $module;
+
+    /**
      * The console command signature.
      *
      * @var string
@@ -51,32 +58,142 @@ class Create extends Command
      */
     public function fire()
     {
-        $module = str_plural(strtolower($this->argument('module')));
-        if ($this->moduleExists($module)) {
-            return $this->error('A module named ['.ucfirst($module).'] already exists.');
+        $this->module = str_plural(ucfirst($this->argument('module')));
+
+        if ($this->moduleExists($this->module)) {
+            return $this->error('A module named ['.$this->module.'] already exists.');
         }
-        $this->generateModuleFromTemplate($module);
+        $this->publishModule();
+        $this->renameModelsAndRepositories();
+        $this->searchAndReplaceInFiles();
+        $this->publishViews();
+        $this->publishMigration();
+        $this->line('-------------------------------');
+        $this->line('<comment>['.$this->module.']</comment> <info>was created in</info> <comment>[/Modules]</comment><info>, customize it!</info>');
+        $this->line('<info>Run the migration with the command</info> <comment>[php artisan migrate]</comment><info>.</info>');
+        $this->line('<info>Add</info> <comment>[TypiCMS\Modules\\'.$this->module.'\Providers\ModuleProvider::class,]</comment>');
+        $this->line('<info>to the providers array in</info> <comment>[config/app.php]</comment><info>.</info>');
+        $this->line('-------------------------------');
     }
 
     /**
-     * Generate the module.
-     *
-     * @param string $module
-     *
-     * @return mixed
+     * Generate the module in Modules directory.
      */
-    private function generateModuleFromTemplate($module)
+    private function publishModule()
     {
         $from = base_path('vendor/typicms/objects/src');
-        $to = base_path('Modules/'.ucfirst($module));
+        $to = base_path('Modules/'.$this->module);
 
         if ($this->files->isDirectory($from)) {
             $this->publishDirectory($from, $to);
         } else {
             $this->error("Can’t locate path: <{$from}>");
         }
+    }
 
-        $this->info('The module ['.ucfirst($module).'] was created, you can now customize it and edit the migration file.');
+    /**
+     * Search and remplace all occurences of ‘Objects’
+     * in all files with the name of the new module.
+     */
+    public function searchAndReplaceInFiles()
+    {
+        $directory = base_path('Modules/'.$this->module);
+
+        $manager = new MountManager([
+            'directory' => new Flysystem(new LocalAdapter($directory))
+        ]);
+
+        $search = [
+            'objects',
+            'object',
+            'Objects',
+            'Object',
+        ];
+        $replace = [
+            strtolower($this->module),
+            strtolower(str_singular($this->module)),
+            $this->module,
+            str_singular($this->module),
+        ];
+
+        foreach ($manager->listContents('directory://', true) as $file) {
+            if ($file['type'] === 'file') {
+                $content = str_replace($search, $replace, $manager->read('directory://'.$file['path']));
+                $manager->put('directory://'.$file['path'], $content);
+            }
+        }
+    }
+
+    /**
+     * Rename models and repositories.
+     *
+     * @param string $module
+     */
+    public function renameModelsAndRepositories()
+    {
+        $moduleDir = base_path('Modules/'.$this->module);
+        $paths = [
+            $moduleDir.'/Models/Object.php',
+            $moduleDir.'/Models/ObjectTranslation.php',
+            $moduleDir.'/Repositories/ObjectInterface.php',
+            $moduleDir.'/Repositories/EloquentObject.php',
+        ];
+        foreach ($paths as $path) {
+            $this->files->move($path, $this->transformFilename($path));
+        }
+    }
+
+    /**
+     * Publish migration file.
+     */
+    public function publishMigration()
+    {
+        $from = base_path('Modules/'.$this->module.'/database/migrations/2016_01_04_225000_create_objects_table.php');
+        $to = database_path('migrations/'.date('Y_m_d_His').'_create_'.strtolower($this->module).'_table.php');
+        $this->publishFile($from, $to);
+    }
+
+    /**
+     * Publish views.
+     */
+    public function publishViews()
+    {
+        $from = base_path('Modules/'.$this->module.'/resources/views');
+        $to = resource_path('views/vendor/'.strtolower($this->module));
+        $this->publishDirectory($from, $to);
+    }
+
+    /**
+     * Rename file in path.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    public function transformFilename($path)
+    {
+        return str_replace('Object', str_singular($this->module), $path);
+    }
+
+    /**
+     * Publish the file to the given path.
+     *
+     * @param string $from
+     * @param string $to
+     *
+     * @return void
+     */
+    protected function publishFile($from, $to)
+    {
+        if ($this->files->exists($to) && ! $this->option('force')) {
+            return;
+        }
+
+        $this->createParentDirectory(dirname($to));
+
+        $this->files->copy($from, $to);
+
+        $this->status($from, $to, 'File');
     }
 
     /**
@@ -96,9 +213,6 @@ class Create extends Command
 
         foreach ($manager->listContents('from://', true) as $file) {
             $path = $file['path'];
-            if (substr($path, 0, 8) === 'database' || substr($path, 0, 15) === 'resources/views') {
-                continue;
-            }
             if ($file['type'] === 'file' && (!$manager->has('to://'.$file['path']) || $this->option('force'))) {
                 $manager->put('to://'.$file['path'], $manager->read('from://'.$file['path']));
             }
@@ -107,10 +221,23 @@ class Create extends Command
         $this->status($from, $to, 'Directory');
     }
 
+    /**
+     * Create the directory to house the published files if needed.
+     *
+     * @param string $directory
+     * @return void
+     */
+    protected function createParentDirectory($directory)
+    {
+        if (! $this->files->isDirectory($directory)) {
+            $this->files->makeDirectory($directory, 0755, true);
+        }
+    }
+
     public function moduleExists($module)
     {
-        $location1 = $this->files->isDirectory(base_path('Modules/'.ucfirst($module)));
-        $location2 = $this->files->isDirectory(base_path('vendor/typicms/'.$module));
+        $location1 = $this->files->isDirectory(base_path('Modules/'.$module));
+        $location2 = $this->files->isDirectory(base_path('vendor/typicms/'.strtolower($module)));
         return $location1 || $location2;
     }
 
