f3334237a77a12e003aa90d1cfcbaed74b2f5b8e
Samuel De Backer <sdebacker@gmail.com>
2016-01-15 12:19:57 +0100

    forms toolbar

--- src/resources/views/admin/_buttons-form.blade.php
+++ src/resources/views/admin/_buttons-form.blade.php
@@ -1,7 +1,8 @@
-    <div class="form-group">
-        <button class="btn-primary btn" value="true" id="exit" name="exit" type="submit">@lang('validation.attributes.save and exit')</button>
-        <button class="btn-default btn" type="submit">@lang('validation.attributes.save')</button>
-        @if (method_exists($model, 'previewUri') && $previewUri = $model->previewUri())
-        <a class="btn btn-default pull-right btn-preview" href="{{ $previewUri }}?preview=true">@lang('validation.attributes.preview')</a>
-        @endif
-    </div>
+<div class="btn-toolbar">
+    <button class="btn-primary btn" value="true" id="exit" name="exit" type="submit">@lang('validation.attributes.save and exit')</button>
+    <button class="btn-default btn" type="submit">@lang('validation.attributes.save')</button>
+    @if (method_exists($model, 'previewUri') && $previewUri = $model->previewUri())
+    <a class="btn btn-default btn-preview" href="{{ $previewUri }}?preview=true">@lang('validation.attributes.preview')</a>
+    @endif
+    @include('core::admin._tabs-lang-form')
+</div>
