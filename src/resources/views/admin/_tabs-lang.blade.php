cd58315a12aa6b5a494b0dd2185ac74b505d7b35
Samuel De Backer <sdebacker@gmail.com>
2016-01-15 17:08:33 +0100

    alert

--- src/resources/views/admin/_tabs-lang.blade.php
+++ src/resources/views/admin/_tabs-lang.blade.php
@@ -1,13 +1,5 @@
 @if (count($locales) > 1)
 <div class="alert alert-warning">
-    <p>The partial <code>core::admin._tabs-lang</code> is deprecated, please remove its inclusion from every <code>_form.blade.php</code> views and from <code>_menulink-form.blade.php</code>.</p>
-    <hr>
-    <ul class="nav nav-tabs" id="locale-changer">
-        @foreach ($locales as $lang)
-        <li class="@if ($lang == $locale)active @endif">
-            <a href="#{{ $lang }}" data-target="#{{ $lang }}" data-locale="{{ $lang }}" data-toggle="tab">@lang('global.languages.'.$lang)</a>
-        </li>
-        @endforeach
-    </ul>
+    <p>The way translated fields are managed has changed, please republish every <code>_form.blade.php</code> partials.</p>
 </div>
 @endif
