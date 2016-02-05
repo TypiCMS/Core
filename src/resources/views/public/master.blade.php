76328553fff114dfd6691441d6aa4a47d59c7022
Samuel De Backer <sdebacker@gmail.com>
2016-02-05 23:05:41 +0100

    IE8 dropped

--- src/resources/views/public/master.blade.php
+++ src/resources/views/public/master.blade.php
@@ -21,11 +21,6 @@
 
     @yield('css')
 
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
     @if(app()->environment('production') and config('typicms.google_analytics_code'))
 
     <script>
