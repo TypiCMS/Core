97f9a9f92dbf34879a42c70e269332e8c66ccb3c
Eldon Ahrold <eahrold@bigclubdigital.com>
2016-02-15 21:32:30 -0600

    Add the ability to specify public api endpoints in Module's HTTP/Controllers/ApiController

--- src/Http/Controllers/BaseApiController.php
+++ src/Http/Controllers/BaseApiController.php
@@ -6,11 +6,18 @@ use Illuminate\Routing\Controller;
 
 abstract class BaseApiController extends Controller
 {
+    
+    /**
+     *  Array of endpoints that do not require authorization.
+     *  
+     */
+    protected $publicEndpoints = [];
+
     protected $repository;
 
     public function __construct($repository = null)
     {
-        $this->middleware('api');
+        $this->middleware('api',['except' => $publicEndpoints]);
         $this->repository = $repository;
     }
 
