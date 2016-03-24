dbb5cc628ec3436354cd47668bfefa76f6bb6241
Samuel De Backer <sdebacker@gmail.com>
2016-03-24 14:38:09 +0100

    no spaces around dot

--- src/resources/views/form/_slug.blade.php
+++ src/resources/views/form/_slug.blade.php
@@ -1,9 +1,9 @@
 @foreach ($locales as $lang)
 <div class="form-group form-group-translation @if($errors->has($lang.'.slug'))has-error @endif">
-    {!! Form::label('<span>'.trans('validation.attributes.slug').'</span> <span>('.$lang.')</span>')->addClass('control-label')->forId($lang . '[slug]') !!}
+    {!! Form::label('<span>'.trans('validation.attributes.slug').'</span> <span>('.$lang.')</span>')->addClass('control-label')->forId($lang.'[slug]') !!}
     <span></span>
     <div class="input-group">
-        {!! Form::text($lang . '[slug]')->addClass('form-control')->id($lang . '[slug]')->data('slug', $lang . '[title]')->data('language', $lang) !!}
+        {!! Form::text($lang.'[slug]')->addClass('form-control')->id($lang.'[slug]')->data('slug', $lang.'[title]')->data('language', $lang) !!}
         <span class="input-group-btn">
             <button class="btn btn-default btn-slug @if($errors->has($lang.'.slug'))btn-danger @endif" type="button">@lang('validation.attributes.generate')</button>
         </span>
