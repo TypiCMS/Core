5d2d2b5ef3836536e74df8e45a7d17a7e0d120bb
Samuel De Backer <sdebacker@gmail.com>
2016-04-09 13:43:18 +0200

    _button-create

---
+++ src/resources/views/admin/_button-create.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ route('admin::create-'.str_singular($module)) }}" class="btn-add" title="@lang($module.'::global.New')">
+    <i class="fa fa-plus-circle"></i><span class="sr-only">@lang($module.'::global.New')</span>
+</a>
