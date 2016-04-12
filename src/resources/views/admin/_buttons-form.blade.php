820c3e55664503a88f6e6db6163d7862e3f4e1b5
Samuel De Backer <sdebacker@gmail.com>
2016-04-12 16:39:10 +0200

    preview button only when module linked to page

--- src/resources/views/admin/_buttons-form.blade.php
+++ src/resources/views/admin/_buttons-form.blade.php
@@ -1,8 +1,8 @@
 <div class="btn-toolbar">
     <button class="btn-primary btn" value="true" id="exit" name="exit" type="submit">@lang('validation.attributes.save and exit')</button>
     <button class="btn-default btn" type="submit">@lang('validation.attributes.save')</button>
-    @if (method_exists($model, 'previewUri') && $previewUri = $model->previewUri())
-    <a class="btn btn-default btn-preview" href="{{ $previewUri }}?preview=true">@lang('validation.attributes.preview')</a>
+    @if ($model->getTable() == 'pages' || TypiCMS::getPageLinkedToModule($model->getTable()))
+    <a class="btn btn-default btn-preview" href="{{ $model->previewUri() }}?preview=true">@lang('validation.attributes.preview')</a>
     @endif
     @include('core::admin._lang-switcher', ['js' => true])
 </div>
