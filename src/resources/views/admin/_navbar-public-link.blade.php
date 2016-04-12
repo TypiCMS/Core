c17e82318b88f2fa506cecc84454353e8746bc3f
Webfactory Bulgaria <accounts@webfactory.bg>
2016-04-12 09:51:03 +0300

    Update _navbar-public-link.blade.php
    No need for this I guess

--- src/resources/views/admin/_navbar-public-link.blade.php
+++ src/resources/views/admin/_navbar-public-link.blade.php
@@ -1,7 +1,7 @@
 @if (isset($model) and $model->id)
 <a href="{{ TypiCMS::isLocaleOnline($locale) ? url($model->uri($locale)) : url($model->uri()) }}">
 @elseif ($module = Request::segment(2) and Route::has($locale.'.'.$module))
-<a href="{{ TypiCMS::isLocaleOnline($locale) ? route($locale.'.'.$module) : route($locale.'.'.$module) }}">
+<a href="{{ route($locale.'.'.$module) }}">
 @else
 <a href="{{ url('/') }}">
 @endif
