8b47b1c5ca7623677f64f78d0a1b2900ac6bc947
Samuel De Backer <sdebacker@gmail.com>
2016-04-18 23:16:55 +0200

    partials

--- src/resources/views/admin/_v-client-table.blade.php
+++ src/resources/views/admin/_v-client-table.blade.php
@@ -1,6 +1,6 @@
 <v-client-table :data="tableData" :columns="columns" :options="options"></v-client-table>
 @section('js')
 <script>
-    vm.tableData = {!! News::allFiltered(config('typicms.news.select', '*'))['data'] !!};
+    vm.tableData = {!! $data['data'] !!};
 </script>
 @endsection
--- src/resources/views/admin/_v-server-table.blade.php
+++ src/resources/views/admin/_v-server-table.blade.php
@@ -1,4 +1,4 @@
 <div class="VueTables__loading" v-if="loading">
     <span class="VueTables__spinner fa fa-spin fa-fw fa-gear fa-3x"></span>
 </div>
-<v-server-table url="{{ route('api::index-news') }}" :columns="columns" :options="options"></v-server-table>
+<v-server-table url="{{ $url }}" :columns="columns" :options="options"></v-server-table>
