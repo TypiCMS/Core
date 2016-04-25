301f853e2233157712269921596ef45b1c66338a
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-04-25 10:02:56 +0200

    Make translatable text "Supprimier" in image fieldset template

--- src/resources/views/admin/_image-fieldset.blade.php
+++ src/resources/views/admin/_image-fieldset.blade.php
@@ -2,7 +2,7 @@
             @if($model->$field)
             <div class="fieldset-preview">
                 {!! $model->present()->thumb(150, 150, ['resize'], $field) !!}
-                <small class="text-danger delete-attachment" data-table="{{ $model->getTable() }}" data-id="{{ $model->id }}" data-field="{{ $field }}">Supprimer</small>
+                <small class="text-danger delete-attachment" data-table="{{ $model->getTable() }}" data-id="{{ $model->id }}" data-field="{{ $field }}">@lang('global.Delete')</small>
             </div>
             @endif
             <div class="fieldset-field">
