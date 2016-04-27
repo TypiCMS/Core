cdef06b2c0b30f20bb5f5a86a7a86d33c1397cbf
Samuel De Backer <sdebacker@gmail.com>
2016-04-27 09:35:40 +0200

    Revert "Merge branch '2.7'"
    This reverts commit 4d1218230b76d7defbd973e94c0f6024284b12ad, reversing
    changes made to c3ede8de4c24025df493b560526a4fff94e1c6f7.

--- src/resources/views/public/_feed-links.blade.php
+++ src/resources/views/public/_feed-links.blade.php
@@ -1,7 +1,3 @@
 @foreach(config('typicms.feeds', []) as $feed)
-<<<<<<< HEAD
     {!! app('feed')->link(route($lang.'.'.$feed['module'].'.feed'), 'atom', trans($feed['module'].'::global.feed').' – '.$websiteTitle, $lang) !!}
-=======
-    {!! Feed::link(route($lang.'.'.$feed['module'].'.feed'), 'atom', trans($feed['module'].'::global.feed').' – '.$websiteTitle, $lang) !!}
->>>>>>> 2.7
 @endforeach
