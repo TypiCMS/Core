be86c93d6b2f37bff9ac5efe77896c8e272d6810
Samuel De Backer <sdebacker@gmail.com>
2016-04-27 23:43:09 +0200

    CollectionExtensions

--- src/Providers/CollectionExtensions.php
+++ src/Providers/CollectionExtensions.php
@@ -12,7 +12,7 @@ class CollectionExtensions extends ServiceProvider
         Collection::macro('translate', function ($locale = null) {
             $locale = $locale ?: app()->getLocale();
 
-            return collect($this->items)->map(function ($item) use ($locale) {
+            return $this->map(function ($item) use ($locale) {
                 foreach ($item->getAttributes() as $key => $value) {
                     if (in_array($key, $item->translatable)) {
                         $item->$key = $item->translate($key, $locale);
