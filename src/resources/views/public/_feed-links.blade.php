81e4743e1cca248c9384e4533473db3784c41ca2
Samuel De Backer <sdebacker@gmail.com>
2016-05-30 22:26:34 +0200

    feed links

--- src/resources/views/public/_feed-links.blade.php
+++ src/resources/views/public/_feed-links.blade.php
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-@foreach(config('typicms.feeds', []) as $feed)
-    @if (Route::has($lang.'.'.$feed['module'].'.feed'))
-        {!! app('feed')->link(route($lang.'.'.$feed['module'].'.feed'), 'atom', trans($feed['module'].'::global.feed').' – '.$websiteTitle, $lang) !!}
-    @endif
-=======
 @foreach(TypiCMS::feeds() as $feed)
     {!! app('feed')->link($feed['url'], 'atom', $feed['title'], $lang) !!}
->>>>>>> master
 @endforeach
