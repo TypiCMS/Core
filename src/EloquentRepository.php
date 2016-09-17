2d8d9ff8db461576ebe3198001f0aedd9579601a
Samuel De Backer <sdebacker@gmail.com>
2016-09-17 13:36:12 +0000

    Applied fixes from StyleCI

--- src/EloquentRepository.php
+++ src/EloquentRepository.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace TypiCMS\Modules\Core;
 
 use Rinvex\Repository\Repositories\EloquentRepository as BaseRepository;
 
 class EloquentRepository extends BaseRepository
 {
-
     /**
      * Make a new instance of the entity to query on.
      *
