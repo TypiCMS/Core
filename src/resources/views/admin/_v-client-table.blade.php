406f09fca8e08a65cbb93c2803a3cd28652174be
Samuel De Backer <sdebacker@gmail.com>
2016-11-04 17:02:48 +0100

    Vuejs removed

--- src/resources/views/admin/_v-client-table.blade.php
+++
@@ -1,6 +0,0 @@
-<v-client-table :data="tableData" :columns="columns" :options="options"></v-client-table>
-@section('js')
-<script>
-    vm.tableData = {!! $data['data'] !!};
-</script>
-@endsection
--- src/resources/views/admin/_v-server-table.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="VueTables__loading" v-if="loading">
-    <span class="VueTables__spinner fa fa-spin fa-fw fa-gear fa-3x"></span>
-</div>
-<v-server-table url="{{ $url }}" :columns="columns" :options="options"></v-server-table>
