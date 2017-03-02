e1f0ae213fe87f78b6adc6f94bf7d54b15df7a01
Samuel De Backer <sdebacker@gmail.com>
2017-03-02 15:08:16 +0100

    select buttons translations

--- src/resources/views/admin/_button-select.blade.php
+++ src/resources/views/admin/_button-select.blade.php
@@ -4,13 +4,13 @@
     </span>
     <button type="button" class="btn btn-default dropdown-toggle" ng-class="{disabled: !displayedModels.length}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="caret"></span>
-        <span class="sr-only">Toggle Dropdown</span>
+        <span class="sr-only">{{ __('Toggle Dropdown') }}</span>
     </button>
     <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-        <li><a ng-click="checkAll()" href="#">All</a></li>
-        <li><a ng-click="uncheckAll()" href="#">None</a></li>
+        <li><a ng-click="checkAll()" href="#">{{ __('All') }}</a></li>
+        <li><a ng-click="uncheckAll()" href="#">{{ __('None') }}</a></li>
         <li role="separator" class="divider"></li>
-        <li><a ng-click="check('status', {[TypiCMS.content_locale]: '1'})" href="#">Online</a></li>
-        <li><a ng-click="check('status', {[TypiCMS.content_locale]: '0'})" href="#">Offline</a></li>
+        <li><a ng-click="check('status', {[TypiCMS.content_locale]: '1'})" href="#">{{ __('Published items') }}</a></li>
+        <li><a ng-click="check('status', {[TypiCMS.content_locale]: '0'})" href="#">{{ __('Unpublished items') }}</a></li>
     </ul>
 </div>
