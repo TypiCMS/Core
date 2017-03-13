88568607ee723f8b1a18f4630c35bac5373ab53c
Samuel De Backer <sdebacker@gmail.com>
2017-03-13 19:27:29 +0100

    image_fieldset

--- src/resources/views/admin/_image-fieldset.blade.php
+++ src/resources/views/admin/_image-fieldset.blade.php
@@ -1,11 +1,21 @@
-        <div class="fieldset-media fieldset-image">
-            @if($model->$field)
-            <div class="fieldset-preview">
-                {!! $model->present()->thumb(150, 150, ['resize'], $field) !!}
-                <small class="text-danger delete-attachment" data-table="{{ $model->getTable() }}" data-id="{{ $model->id }}" data-field="{{ $field }}">@lang('Delete')</small>
-            </div>
-            @endif
-            <div class="fieldset-field">
-                {!! BootForm::file(__(''.$field), $field) !!}
-            </div>
+<div class="filepicker" id="filepicker">
+    <div class="filepicker-content">
+        @include('files::admin._filemanager', ['options' => ['dropzoneHidden', 'addButton']])
+    </div>
+</div>
+<div class="form-group">
+    <label class="control-label">Image</label>
+    @if($model->$field)
+    <div class="filemanager-item-wrapper">
+        <div class="filemanager-item-icon">
+            <img class="filemanager-item-image" src="{{ $model->$field->thumb_sm }}" alt="{{ $model->$field->alt_attribute_translated }}">
         </div>
+        <button class="btn btn-xs btn-danger delete-attachment" data-table="{{ $model->getTable() }}" data-id="{{ $model->id }}" data-field="{{ $field }}" type="button">{{ __('Delete') }}</button>
+        <button class="btn btn-xs btn-default replace-attachment" data-table="{{ $model->getTable() }}" data-id="{{ $model->id }}" data-field="{{ $field }}" type="button">{{ __('Replace') }}</button>
+    </div>
+    @else
+        <p>
+            <button class="btn btn-success add-attachment" id="select-files" data-table="{{ $model->getTable() }}" data-id="{{ $model->id }}" data-field="{{ $field }}" type="button">{{ __('Add image') }}</button>
+        </p>
+    @endif
+</div>
