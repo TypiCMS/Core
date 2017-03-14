45d9f822b4da444abd0fefee71116ed5a8e492ab
gjportegies <Guus.Portegies@hva.nl>
2017-03-14 10:55:58 +0100

    Added Dutch translation

---
+++ src/resources/lang/nl/global.php
@@ -0,0 +1,72 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | EN Global TypiCMS Language Lines
+    |--------------------------------------------------------------------------
+    */
+    'languages' => [
+        'fr' => 'Frans',
+        'nl' => 'Nederlands',
+        'en' => 'Engels',
+        'es' => 'Spaans',
+    ],
+    'form' => [
+        'page content' => 'pagina inhoud',
+        'meta data'    => 'metadata',
+        'options'      => 'opties',
+    ],
+    'menus' => [
+        'content'  => 'Inhoud',
+        'users'    => 'Gebruikers en groepen',
+        'media'    => 'Media',
+        'contacts' => 'Contacten',
+    ],
+    'Index'        => 'Lijst',
+    'Create'       => 'Maken',
+    'Store'        => 'Opslaan',
+    'Update'       => 'Update',
+    'Delete'       => 'Verwijder',
+    'Select all'   => 'Selecteer alles',
+    'Deselect all' => 'Deselecteer alles',
+    'Online'       => 'Online',
+    'Offline'      => 'Offline',
+    'View'         => 'Bekijk',
+    'Sort'         => 'Sorteer',
+    'Edit'         => 'Bewerken',
+    'Not found'    => 'Niet found',
+    'Untitled'     => 'Geen titel',
+
+    'The form contains errors' => 'Het formulier bevat fouten',
+
+    'Previous' => 'Vorige',
+    'Next'     => 'Volgende',
+
+    'Yes' => 'Ja',
+    'No'  => 'Nee',
+
+    'Home'                  => 'Home',
+    'En ligne/Hors ligne'   => 'Online/Offline',
+    'No default page found' => 'Geen standaard pagina gevonden',
+    'No file'               => 'Geen bestand',
+    'Settings'              => 'Instellingen',
+    'Admin side'            => 'Administratie paneel',
+    'View website'          => 'Bekijk website',
+
+    'Mandatory fields' => 'Verplichte velden',
+
+    // Tabs
+    'Content'   => 'Inhoud',
+    'Meta'      => 'Meta',
+    'Options'   => 'Opties',
+    'Galleries' => 'Galerij',
+    'Files'     => 'Bestanden',
+    'Images'    => 'Afbeeldingen',
+    'Info'      => 'Info',
+
+    'Toggle navigation' => 'Toggle navigatie',
+
+    'Items sorted' => 'Items gesorteerd.',
+];
