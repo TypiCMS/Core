b32f73d29fc2b6cf23fdc3f148c530b8e7092dc7
Samuel De Backer <sdebacker@gmail.com>
2017-03-15 16:00:51 +0100

    FileController

---
+++ src/resources/assets/typicms/controllers/FileController.js
@@ -0,0 +1,40 @@
+/*jslint browser: true*/
+/*globals $, jQuery, angular, TypiCMS, alertify, top, window, console*/
+
+(function (angular) {
+
+    'use strict';
+
+    angular.module('typicms').controller('FileController', ['$http', '$scope', '$rootScope', '$attrs', '$api', function ($http, $scope, $rootScope, $attrs, $api) {
+
+        $scope.table = $attrs.table;
+        $scope.id = $attrs.id;
+        $scope.field = $attrs.field;
+        $scope.model = TypiCMS.model;
+
+        $scope.$on('fileAdded', function (event, model) {
+            $scope.model.image_id = model.id;
+            $scope.model.image = model;
+            $('#image_id').val(model.id);
+        });
+
+        /**
+         * Remove file
+         */
+        $scope.removeImage = function (model) {
+            $scope.model.image_id = null;
+            $scope.model.image = null;
+        };
+
+        /**
+         * Add file
+         */
+        $scope.addImage = function () {
+            $('html, body').addClass('noscroll');
+            $('#filepicker').addClass('filepicker-open');
+        }
+
+
+    }]);
+
+}(angular));
