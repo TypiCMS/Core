124f1df7e1ef500533c0e56fe9be112ab7b4e09c
Samuel De Backer <sdebacker@gmail.com>
2017-03-16 12:16:16 +0100

    remove button for a file

--- src/resources/views/admin/_image-fieldset.blade.php
+++ src/resources/views/admin/_image-fieldset.blade.php
@@ -9,12 +9,11 @@
         {!! Bootform::hidden($field.'_id')->id($field.'_id') !!}
 
         <div ng-app="typicms" ng-cloak ng-controller="FileController">
-            <div ng-show="model.image" class="filemanager-item-wrapper">
-                <div class="filemanager-item-icon">
+            <div ng-show="model.image" class="filemanager-item-removable">
+                <a class="filemanager-item-removable-button" ng-click="removeImage(model)" href="#"><span class="fa fa-times"></span></a>
+                <div class="filemanager-item-image-wrapper">
                     <img class="filemanager-item-image" ng-src="@{{ model.image.thumb_sm }}" alt="@{{ model.image.alt_attribute_translated }}">
                 </div>
-                <button class="btn btn-xs btn-danger delete-attachment" ng-click="removeImage(model)" data-message="{{ __('Delete this image?') }}" data-field="{{ $field }}_id" type="button">{{ __('Delete') }}</button>
-                <button class="btn btn-xs btn-default replace-attachment" ng-click="addImage(model)" type="button">{{ __('Replace') }}</button>
             </div>
             <button ng-hide="model.image" ng-click="addImage(model)" class="btn btn-success add-attachment" type="button">{{ __('Add an image') }}</button>
         </div>
