0be64adb2c50092f3ef67e60c7e975043b6af1ef
Samuel De Backer <sdebacker@gmail.com>
2017-03-31 20:13:21 +0200

    keep file name on upload

--- src/Services/Upload/FileUpload.php
+++ src/Services/Upload/FileUpload.php
@@ -4,6 +4,7 @@ namespace TypiCMS\Modules\Core\Services\Upload;
 
 use Exception;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -24,14 +25,22 @@ class FileUpload
      */
     public function handle(UploadedFile $file, $path = 'public/files')
     {
+        $fileName = Str::slug(pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME));
+
         $fileInfo = [];
-        $fileInfo['original_name'] = $file->getClientOriginalName();
-        $fileInfo['path'] = $file->store($path);
         $fileInfo['filesize'] = $file->getClientSize();
         $fileInfo['mimetype'] = $file->getClientMimeType();
+        $fileInfo['extension'] = $file->getClientOriginalExtension();
+        $fileInfo['filename'] = $fileName.'.'.$fileInfo['extension'];
         list($fileInfo['width'], $fileInfo['height']) = getimagesize($file);
+
+        $filecounter = 1;
+        while (Storage::has($path.'/'.$fileInfo['filename'])) {
+            $fileInfo['filename'] = $fileName.'_'.$filecounter++.'.'.$fileInfo['extension'];
+        }
+        $fileInfo['path'] = $file->storeAs($path, $fileInfo['filename']);
         $fileInfo['filename'] = pathinfo($fileInfo['path'], PATHINFO_BASENAME);
-        $fileInfo['extension'] = pathinfo($fileInfo['path'], PATHINFO_EXTENSION);
+
         try {
             $fileInfo['type'] = config('file.types')[$fileInfo['extension']];
         } catch (Exception $e) {
