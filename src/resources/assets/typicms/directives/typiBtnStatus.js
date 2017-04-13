dbe15f2e37e43b405fde05a501ab90f4f082cf56
Samuel De Backer <sdebacker@gmail.com>
2017-04-13 21:51:30 +0200

    some styling for nested lists

--- src/resources/assets/typicms/directives/typiBtnStatus.js
+++ src/resources/assets/typicms/directives/typiBtnStatus.js
@@ -5,8 +5,8 @@ angular.module('typicms').directive('typiBtnStatus', function() {
             locale: '=locale',
             action: '&'
         },
-        template: '<div class="btn btn-xs btn-link" ng-click="action()">' +
-                '<span class="fa switch" ng-class="model.status.'+TypiCMS.content_locale+' == \'1\' ? \'fa-toggle-on\' : \'fa-toggle-off\'"></span>' +
+        template: '<div class="btn btn-xs btn-link btn-status" ng-click="action()">' +
+                '<span class="fa btn-status-switch" ng-class="model.status.'+TypiCMS.content_locale+' == \'1\' ? \'fa-toggle-on\' : \'fa-toggle-off\'"></span>' +
                 '<span class="sr-only" ng-show="model.status.'+TypiCMS.content_locale+' == \'1\'" translate>Published</span>' +
                 '<span class="sr-only" ng-hide="model.status.'+TypiCMS.content_locale+' == \'0\'" translate>Unpublished</span>' +
             '</div>'
