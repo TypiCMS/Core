b21edaab407727ee7ec26f3067c4b46baa3195c9
Samuel De Backer <sdebacker@gmail.com>
2017-04-18 15:29:54 +0200

    translations

--- src/resources/views/admin/_lang-switcher-for-list.blade.php
+++ src/resources/views/admin/_lang-switcher-for-list.blade.php
@@ -1,11 +1,11 @@
 @if (count($locales) > 1)
     <div class="btn-group pull-right">
         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span id="active-locale">@lang('global.languages.'.$locale)</span> <span class="caret"></span>
+            <span id="active-locale">@lang('languages.'.$locale)</span> <span class="caret"></span>
         </button>
         <ul class="dropdown-menu">
             @foreach ($locales as $lang)
-            <li><a class="@if($lang == $locale)active @endif" href="?{{ http_build_query(Request::except('locale') + ['locale' => $lang]) }}" data-locale="{{ $lang }}">@lang('global.languages.'.$lang)</a></li>
+            <li><a class="@if($lang == $locale)active @endif" href="?{{ http_build_query(Request::except('locale') + ['locale' => $lang]) }}" data-locale="{{ $lang }}">@lang('languages.'.$lang)</a></li>
             @endforeach
         </ul>
     </div>
