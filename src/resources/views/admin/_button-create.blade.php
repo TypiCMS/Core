04aa3a00e93c85b31598d9a63d3ae25c2c105cea
Samuel De Backer <sdebacker@gmail.com>
2017-04-20 19:37:14 +0200

    No create button when user is not allowed to create.

--- src/resources/views/admin/_button-create.blade.php
+++ src/resources/views/admin/_button-create.blade.php
@@ -1,3 +1,5 @@
+@can('create-'.str_singular($module))
 <a href="{{ route('admin::create-'.str_singular($module)) }}" class="btn-add" title="@lang($module.'::global.New')">
     <i class="fa fa-plus-circle"></i><span class="sr-only">@lang($module.'::global.New')</span>
 </a>
+@endcan
