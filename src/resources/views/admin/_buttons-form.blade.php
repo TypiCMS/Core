16c1253588f8453546f92c3eef64935ce5af0758
Samuel De Backer <sdebacker@gmail.com>
2017-05-02 18:13:45 +0200

    Button to add files

--- src/resources/views/admin/_buttons-form.blade.php
+++ src/resources/views/admin/_buttons-form.blade.php
@@ -4,6 +4,9 @@
     @if ($model->getTable() == 'pages' || TypiCMS::getPageLinkedToModule($model->getTable()))
     <a class="btn btn-default btn-preview" href="{{ $model->previewUri() }}?preview=true">{{ __('Preview') }}</a>
     @endif
+    @if (method_exists($model, 'files'))
+    <button class="btn btn-success" id="select-files" type="button" {!! $model->id ? '' : 'disabled title="'.__('Save this item first, then add files.').'"' !!}>{{ __('Add files') }}</button>
+    @endif
     {{ $slot }}
     @include('core::admin._lang-switcher-for-form')
 </div>
