1ead8abd5a01cbfe5ac74e0c988ae34972cdce13
Samuel De Backer <sdebacker@gmail.com>
2017-06-14 12:25:53 +0200

    lang switcher

--- src/resources/views/public/_lang-switcher.blade.php
+++ src/resources/views/public/_lang-switcher.blade.php
@@ -9,18 +9,18 @@
         <li class="lang-switcher-item @if ($locale == config('app.locale'))active @endif" role="menuitem">
             @if (isset($model) and isset($page))
                 @if ($model->category and $model->translate('status', $locale))
-                    <a class="lang-switcher-link href="{{ url($page->uri($locale).'/'.$model->category->translate('slug', $locale).'/'.$model->translate('slug', $locale)) }}">{{ $locale }}</a>
+                    <a class="lang-switcher-link" href="{{ url($page->uri($locale).'/'.$model->category->translate('slug', $locale).'/'.$model->translate('slug', $locale)) }}">{{ $locale }}</a>
                 @elseif ($model->translate('status', $locale))
-                    <a class="lang-switcher-link href="{{ url($page->uri($locale).'/'.$model->translate('slug', $locale)) }}">{{ $locale }}</a>
+                    <a class="lang-switcher-link" href="{{ url($page->uri($locale).'/'.$model->translate('slug', $locale)) }}">{{ $locale }}</a>
                 @else
-                    <a class="lang-switcher-link href="{{ url($page->uri($locale)) }}">{{ $locale }}</a>
+                    <a class="lang-switcher-link" href="{{ url($page->uri($locale)) }}">{{ $locale }}</a>
                 @endif
-            @elseif (isset($category))
-            <a class="lang-switcher-link href="{{ url($page->uri($locale).'/'.$category->translate('slug', $locale)) }}">{{ $locale }}</a>
+            @elseif (isset($page) && Route::current()->hasParameter('category'))
+            <a class="lang-switcher-link" href="{{ url($page->uri($locale).'/'.$category->translate('slug', $locale)) }}">{{ $locale }}</a>
             @elseif (isset($page))
-            <a class="lang-switcher-link href="{{ url($page->uri($locale)) }}">{{ $locale }}</a>
+            <a class="lang-switcher-link" href="{{ url($page->uri($locale)) }}">{{ $locale }}</a>
             @else
-            <a class="lang-switcher-link href="{{ url('/') }}">{{ $locale }}</a>
+            <a class="lang-switcher-link" href="{{ url('/') }}">{{ $locale }}</a>
             @endif
         </li>
         @endforeach
