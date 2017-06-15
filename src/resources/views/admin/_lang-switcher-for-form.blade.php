b2d44a1e82385247a2ce0b49e9a5e77634357163
Samuel De Backer <sdebacker@gmail.com>
2017-06-15 22:35:11 +0200

    translations

--- src/resources/views/admin/_lang-switcher-for-form.blade.php
+++ src/resources/views/admin/_lang-switcher-for-form.blade.php
@@ -1,14 +1,14 @@
 @if (count($locales) > 1)
     <div class="btn-group pull-right">
         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span id="active-locale">@lang('languages.'.($locale?:'all'))</span> <span class="caret"></span>
+            <span id="active-locale">{{ $locale ? : __('All languages') }}</span> <span class="caret"></span>
         </button>
         <ul class="dropdown-menu">
             @foreach ($locales as $lang)
             <li><a class="btn-lang-js @if (!session('allLocalesInForm') && $lang == $locale)active @endif" href="#" data-locale="{{ $lang }}">@lang('languages.'.$lang)</a></li>
             @endforeach
             <li class="divider"></li>
-            <li><a class="btn-lang-js @if (session('allLocalesInForm'))active @endif" href="#" data-locale="all">@lang('languages.all')</a></li>
+            <li><a class="btn-lang-js @if (session('allLocalesInForm'))active @endif" href="#" data-locale="all">@lang('All languages')</a></li>
         </ul>
     </div>
 @endif
