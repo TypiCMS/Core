a9de4aaefca5cace7f8dd55d16703522fa6f2e44
Samuel De Backer <sdebacker@gmail.com>
2017-06-16 12:06:18 +0200

    modal windows

--- src/resources/assets/js/admin/preview-window.js
+++ src/resources/assets/js/admin/preview-window.js
@@ -1,11 +1,51 @@
 $(function () {
-    $('.btn-preview').fancybox({
-        type: 'iframe',
-        iframe: {
-            css: {
-                height: '90vh',
-                width: '90vw'
-            }
-        }
+
+    /**
+     * Build modal window
+     */
+    $('<div>', {
+        id: 'preview-window',
+        class: 'typicms-modal',
+    }).appendTo('body');
+
+    $('<div>', {
+        id: 'preview-window-wrapper',
+        class: 'typicms-modal-wrapper',
+    }).appendTo('#preview-window');
+
+    $('<iframe>', {
+        src: this.href,
+        id: 'preview-content',
+        class: 'typicms-modal-content',
+        frameborder: 0,
+    }).prependTo('#preview-window-wrapper');
+
+    $('<button>', {
+        id: 'close-preview',
+        class: 'typicms-modal-btn-close',
+    }).appendTo('#preview-window');
+
+    $('<span>', {
+        class: 'fa fa-close',
+    }).appendTo('#close-preview');
+
+    /**
+     * Open preview window
+     */
+    $('.btn-preview').on('click', function (event) {
+        event.preventDefault();
+        $('#preview-content').attr({'src': this.href})
+        $('html, body').addClass('noscroll');
+        $('#preview-window').addClass('typicms-modal-open');
+    });
+
+    /**
+     * Close preview window
+     */
+    $('body').on('click', '#close-preview', function(event) {
+        event.preventDefault();
+        $('#preview-content').attr({'src': ''})
+        $('html, body').removeClass('noscroll');
+        $('#preview-window').removeClass('typicms-modal-open');
     });
 });
