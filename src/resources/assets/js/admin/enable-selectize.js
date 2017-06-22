478e1708816405c5f2ad7c5e53ffb887fc11e085
Samuel De Backer <sdebacker@gmail.com>
2017-06-22 14:00:33 +0200

    tags selectize

--- src/resources/assets/js/admin/enable-selectize.js
+++ src/resources/assets/js/admin/enable-selectize.js
@@ -8,14 +8,29 @@ $(function () {
     /**
      * Selectize for tags
      */
-    $('#tags').selectize({
-        persist: false,
-        create: true,
-        options: TypiCMS.tags,
-        searchField: ['item'],
-        labelField: 'item',
-        valueField: 'item',
-        createOnBlur: true
-    });
+    if ($('#tags').length) {
+        $.getJSON({
+            url: '/admin/tags'
+        }).done(function(data) {
+            var tags = data.map(function(x) { return { item: x.tag }; });
+            var oldTags = $('#tags').val().split(',');
+            for (var i = oldTags.length - 1; i >= 0; i--) {
+                if (oldTags[i] !== '') {
+                    tags.push({ 'item': oldTags[i] });
+                }
+            }
+            $('#tags').selectize({
+                persist: false,
+                create: true,
+                options: tags,
+                searchField: 'item',
+                labelField: 'item',
+                valueField: 'item',
+                createOnBlur: true
+            });
+        }).fail(function () {
+            alertify.error('An error occurred while getting tags.');
+        });
+    }
 
 });
