358fa322ceb963f63a47a2fdd8b04ff8b6a692eb
Samuel De Backer <sdebacker@gmail.com>
2017-09-01 14:46:24 +0200

    sidebar.js

--- src/resources/assets/js/admin/sidebar.js
+++ src/resources/assets/js/admin/sidebar.js
@@ -1,6 +1,16 @@
-import updatepreferences from './preferences';
-
 $(function () {
+
+    function updatepreferences(key, value) {
+        var data = {};
+        data[key] = value;
+        $.ajax({
+            type: 'POST',
+            url: '/admin/users/current/updatepreferences',
+            data: data
+        }).fail(function () {
+            alertify.error('User preference couldn’t be set.');
+        });
+    }
 
     $('.panel-collapse').on('hide.bs.collapse', function () {
         updatepreferences('menus_' + $(this).attr('id') + '_collapsed', 'true');
