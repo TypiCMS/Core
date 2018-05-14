89512056afa6df0d426aa5cf6a85fbc221fa18d5
Samuel De Backer <sdebacker@gmail.com>
2018-05-14 10:43:20 +0200

    fix minification problems on angularjs filters

--- src/resources/assets/typicms/filters/dateFromMySQLFilter.js
+++ src/resources/assets/typicms/filters/dateFromMySQLFilter.js
@@ -1,9 +1,12 @@
-angular.module('typicms').filter('dateFromMySQL', function(dateFilter) {
-    return function(date, format) {
-        if (! date) {
-            return;
-        }
-        date = new Date(date.replace(/-/g, '/'));
-        return dateFilter(date, format);
-    };
-});
+angular.module('typicms').filter('dateFromMySQL', [
+    'dateFilter',
+    function(dateFilter) {
+        return function(date, format) {
+            if (!date) {
+                return;
+            }
+            date = new Date(date.replace(/-/g, '/'));
+            return dateFilter(date, format);
+        };
+    },
+]);
