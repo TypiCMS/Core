df4271c2c24c32c869db1d71fd860d4b56c3d3b7
Samuel De Backer <sdebacker@gmail.com>
2018-05-17 20:51:33 +0200

    better markup for dropdown

--- src/resources/views/admin/_button-select.blade.php
+++ src/resources/views/admin/_button-select.blade.php
@@ -2,15 +2,14 @@
     <span class="btn btn-light" ng-class="{disabled: !displayedModels.length}">
         <input type="checkbox" ng-checked="allChecked && displayedModels.length && checked.models.length === displayedModels.length" ng-model="allChecked" ng-click="toggleCheckAll(allChecked)">
     </span>
-    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" ng-class="{disabled: !displayedModels.length}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" ng-class="{disabled: !displayedModels.length}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="selectMenu">
         <span class="caret"></span>
-        <span class="sr-only">{{ __('Toggle Dropdown') }}</span>
     </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-        <a class="dropdown-item" ng-click="checkAll()" href="#">{{ __('All') }}</a>
-        <a class="dropdown-item" ng-click="uncheckAll()" href="#">{{ __('None') }}</a>
+    <div class="dropdown-menu" aria-labelledby="selectMenu">
+        <button type="button" class="dropdown-item" ng-click="checkAll()" href="#">{{ __('All') }}</button>
+        <button type="button" class="dropdown-item" ng-click="uncheckAll()" href="#">{{ __('None') }}</button>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item" ng-click="check('status', {[TypiCMS.content_locale]: '1'})" href="#">{{ __('Published items') }}</a>
-        <a class="dropdown-item" ng-click="check('status', {[TypiCMS.content_locale]: '0'})" href="#">{{ __('Unpublished items') }}</a>
-    </ul>
+        <button type="button" class="dropdown-item" ng-click="check('status', {[TypiCMS.content_locale]: '1'})" href="#">{{ __('Published items') }}</button>
+        <button type="button" class="dropdown-item" ng-click="check('status', {[TypiCMS.content_locale]: '0'})" href="#">{{ __('Unpublished items') }}</button>
+    </div>
 </div>
