2bbccd7fef5d976f72cc1e233cf208d8cc911fb6
Samuel De Backer <sdebacker@gmail.com>
2018-05-17 22:55:04 +0200

    prettier on selectize file

--- src/resources/assets/js/admin/enable-selectize.js
+++ src/resources/assets/js/admin/enable-selectize.js
@@ -1,5 +1,4 @@
-$(function () {
-
+$(function() {
     $('select#category_id').selectize();
     $('select#page_id').selectize();
     $('select#target').selectize();
@@ -9,27 +8,32 @@ $(function () {
      */
     if ($('#tags').length) {
         $.getJSON({
-            url: '/admin/tags'
-        }).done(function(data) {
-            var tags = data.map(function(x) { return { item: x.tag }; });
-            var oldTags = $('#tags').val().split(',');
-            for (var i = oldTags.length - 1; i >= 0; i--) {
-                if (oldTags[i] !== '') {
-                    tags.push({ 'item': oldTags[i] });
+            url: '/admin/tags',
+        })
+            .done(function(data) {
+                var tags = data.map(function(x) {
+                    return { item: x.tag };
+                });
+                var oldTags = $('#tags')
+                    .val()
+                    .split(',');
+                for (var i = oldTags.length - 1; i >= 0; i--) {
+                    if (oldTags[i] !== '') {
+                        tags.push({ item: oldTags[i] });
+                    }
                 }
-            }
-            $('#tags').selectize({
-                persist: false,
-                create: true,
-                options: tags,
-                searchField: 'item',
-                labelField: 'item',
-                valueField: 'item',
-                createOnBlur: true
+                $('#tags').selectize({
+                    persist: false,
+                    create: true,
+                    options: tags,
+                    searchField: 'item',
+                    labelField: 'item',
+                    valueField: 'item',
+                    createOnBlur: true,
+                });
+            })
+            .fail(function() {
+                alertify.error('An error occurred while getting tags.');
             });
-        }).fail(function () {
-            alertify.error('An error occurred while getting tags.');
-        });
     }
-
 });
