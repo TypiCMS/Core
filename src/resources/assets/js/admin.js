014a9fe537332604b56daa68297a81d9e2a4703d
Samuel De Backer <sdebacker@gmail.com>
2018-05-17 22:18:39 +0200

    axios, date filter, popper.js, etc.

--- src/resources/assets/js/admin.js
+++ src/resources/assets/js/admin.js
@@ -1,12 +1,29 @@
-/**
- * jQuery
- */
-window.$ = window.jQuery = require('jquery');
+// window._ = require('lodash');
+window.Popper = require('popper.js').default;
 
 /**
- * Bootstrap
+ * jQuery and the Bootstrap jQuery plugin
  */
-require('bootstrap');
+try {
+    window.$ = window.jQuery = require('jquery');
+    require('bootstrap');
+} catch (e) {}
+
+/**
+ * Axios HTTP library
+ */
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * CSRF Token
+ */
+let token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 /**
  * Dropzone
@@ -18,15 +35,42 @@ window.Dropzone = require('dropzone');
  */
 import Vue from 'vue';
 window.Vue = Vue;
+
+/**
+ * DraggableTree
+ */
 import { DraggableTree } from 'vue-draggable-nested-tree';
 window.DraggableTree = DraggableTree;
 import SlVueTree from 'sl-vue-tree';
 window.SlVueTree = SlVueTree;
 
-import ItemList from './components/ItemList.js';
-if (document.getElementById('itemList')) {
-    new Vue(ItemList);
-}
+/**
+ * i18n
+ */
+import VueI18n from 'vue-i18n';
+import fr from '../../lang/fr.json';
+const messages = { fr };
+const i18n = new VueI18n({ locale: 'fr', messages });
+
+/**
+ * Date filter
+ */
+import date from './filters/Date.js';
+Vue.filter('date', date);
+
+import TypiBtnStatus from './components/TypiBtnStatus.vue';
+
+/**
+ * Lists
+ */
+import ItemList from './components/ItemList.vue';
+new Vue({
+    i18n,
+    components: {
+        ItemList,
+        TypiBtnStatus,
+    },
+}).$mount('#app');
 
 /**
  * Alertify
