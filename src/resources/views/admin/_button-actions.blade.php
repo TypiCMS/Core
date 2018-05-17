8ff8fce7e53924ee7f9de1e5bf0e15ec7c5f0eec
Samuel De Backer <sdebacker@gmail.com>
2018-05-17 17:11:31 +0200

    [wip] vueification

--- src/resources/views/admin/_button-actions.blade.php
+++ src/resources/views/admin/_button-actions.blade.php
@@ -1,21 +1,21 @@
 <div class="btn-group">
-    <button class="btn btn-light dropdown-toggle" ng-class="{disabled: !checked.models.length}" type="button" id="dropdownActions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    <button class="btn btn-light dropdown-toggle" type="button" id="dropdownActions" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
         {{ __('Actions') }}
         <span class="caret"></span>
-        <span class="fa fa-spinner fa-spin fa-fw" ng-show="loading"></span>
+        <span class="fa fa-spinner fa-spin fa-fw"></span>
     </button>
     <ul class="dropdown-menu" aria-labelledby="dropdownActions">
         @if (!isset($only) || in_array('online', $only) )
-        <a class="dropdown-item" ng-click="setItems('status', {[TypiCMS.content_locale]: '1'}, 'publish')" href="#">{{ __('Publish') }}</a>
+        <a class="dropdown-item" @click="setItems('status', {[TypiCMS.content_locale]: '1'}, 'publish')" href="#">{{ __('Publish') }}</a>
         @endif
         @if (!isset($only) || in_array('offline', $only) )
-        <a class="dropdown-item" ng-click="setItems('status', {[TypiCMS.content_locale]: '0'}, 'unpublish')" href="#">{{ __('Unpublish') }}</a>
+        <a class="dropdown-item" @click="setItems('status', {[TypiCMS.content_locale]: '0'}, 'unpublish')" href="#">{{ __('Unpublish') }}</a>
         <div class="dropdown-divider"></div>
         @endif
         @if (!isset($only) || in_array('delete', $only) )
-        <a class="dropdown-item" ng-click="deleteChecked()" href="#">{{ __('Delete') }}</a>
+        <a class="dropdown-item" @click="deleteChecked()" href="#">{{ __('Delete') }}</a>
         @endif
         <div role="separator" class="divider"></div>
-        <a class="dropdown-item disabled" href="#">@{{ checked.models.length }} {{ __('items selected') }}</a>
+        <a class="dropdown-item disabled" href="#">0 {{ __('items selected') }}</a>
     </ul>
 </div>
--- src/resources/views/admin/_button-select.blade.php
+++ src/resources/views/admin/_button-select.blade.php
@@ -1,16 +1,15 @@
 <div class="btn-group mr-2">
-    <span class="btn btn-light" ng-class="{disabled: !displayedModels.length}">
-        <input type="checkbox" ng-checked="allChecked && displayedModels.length && checked.models.length === displayedModels.length" ng-model="allChecked" ng-click="toggleCheckAll(allChecked)">
+    <span class="btn btn-light" :class="{disabled: !filteredModels.length}">
+        <input type="checkbox" :checked="allChecked && filteredModels.length && checkedModels.length === filteredModels.length" :model="allChecked" @click="toggleCheckAll(allChecked)">
     </span>
-    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" ng-class="{disabled: !displayedModels.length}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" :class="{disabled: !filteredModels.length}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="caret"></span>
-        <span class="sr-only">{{ __('Toggle Dropdown') }}</span>
     </button>
     <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-        <a class="dropdown-item" ng-click="checkAll()" href="#">{{ __('All') }}</a>
-        <a class="dropdown-item" ng-click="uncheckAll()" href="#">{{ __('None') }}</a>
+        <a class="dropdown-item" @click="checkAll()" href="#">{{ $t('All') }}</a>
+        <a class="dropdown-item" @click="uncheckAll()" href="#">{{ $t('None') }}</a>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item" ng-click="check('status', {[TypiCMS.content_locale]: '1'})" href="#">{{ __('Published items') }}</a>
-        <a class="dropdown-item" ng-click="check('status', {[TypiCMS.content_locale]: '0'})" href="#">{{ __('Unpublished items') }}</a>
+        <a class="dropdown-item" @click="check('status', {[TypiCMS.content_locale]: '1'})" href="#">{{ $t('Published items') }}</a>
+        <a class="dropdown-item" @click="check('status', {[TypiCMS.content_locale]: '0'})" href="#">{{ $t('Unpublished items') }}</a>
     </ul>
 </div>
