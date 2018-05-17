978e7e37c9523ac77e427e4d111da764b60192e7
Samuel De Backer <sdebacker@gmail.com>
2018-05-17 20:53:58 +0200

    more better markup

--- src/resources/views/admin/_button-actions.blade.php
+++ src/resources/views/admin/_button-actions.blade.php
@@ -4,18 +4,18 @@
         <span class="caret"></span>
         <span class="fa fa-spinner fa-spin fa-fw" ng-show="loading"></span>
     </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownActions">
+    <div class="dropdown-menu" aria-labelledby="dropdownActions">
         @if (!isset($only) || in_array('online', $only) )
-        <a class="dropdown-item" ng-click="setItems('status', {[TypiCMS.content_locale]: '1'}, 'publish')" href="#">{{ __('Publish') }}</a>
+        <button type="button" class="dropdown-item" ng-click="setItems('status', {[TypiCMS.content_locale]: '1'}, 'publish')" href="#">{{ __('Publish') }}</button>
         @endif
         @if (!isset($only) || in_array('offline', $only) )
-        <a class="dropdown-item" ng-click="setItems('status', {[TypiCMS.content_locale]: '0'}, 'unpublish')" href="#">{{ __('Unpublish') }}</a>
+        <button type="button" class="dropdown-item" ng-click="setItems('status', {[TypiCMS.content_locale]: '0'}, 'unpublish')" href="#">{{ __('Unpublish') }}</button>
         <div class="dropdown-divider"></div>
         @endif
         @if (!isset($only) || in_array('delete', $only) )
-        <a class="dropdown-item" ng-click="deleteChecked()" href="#">{{ __('Delete') }}</a>
+        <button type="button" class="dropdown-item" ng-click="deleteChecked()" href="#">{{ __('Delete') }}</button>
         @endif
         <div role="separator" class="divider"></div>
-        <a class="dropdown-item disabled" href="#">@{{ checked.models.length }} {{ __('items selected') }}</a>
-    </ul>
+        <button type="button" class="dropdown-item disabled" href="#">@{{ checked.models.length }} {{ __('items selected') }}</button>
+    </div>
 </div>
--- src/resources/views/admin/_button-select.blade.php
+++ src/resources/views/admin/_button-select.blade.php
@@ -2,10 +2,10 @@
     <span class="btn btn-light" ng-class="{disabled: !displayedModels.length}">
         <input type="checkbox" ng-checked="allChecked && displayedModels.length && checked.models.length === displayedModels.length" ng-model="allChecked" ng-click="toggleCheckAll(allChecked)">
     </span>
-    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" ng-class="{disabled: !displayedModels.length}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="selectMenu">
+    <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" ng-class="{disabled: !displayedModels.length}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dropdownSelect">
         <span class="caret"></span>
     </button>
-    <div class="dropdown-menu" aria-labelledby="selectMenu">
+    <div class="dropdown-menu" aria-labelledby="dropdownSelect">
         <button type="button" class="dropdown-item" ng-click="checkAll()" href="#">{{ __('All') }}</button>
         <button type="button" class="dropdown-item" ng-click="uncheckAll()" href="#">{{ __('None') }}</button>
         <div class="dropdown-divider"></div>
