66a341714d6a562d3c9e995c629e25e7eec52bf0
Samuel De Backer <sdebacker@gmail.com>
2018-05-23 18:13:19 +0200

    Base api controller

---
+++ src/Http/Controllers/BaseApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TypiCMS\Modules\Core\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+abstract class BaseApiController extends Controller
+{
+    protected $repository;
+
+    public function __construct($repository = null)
+    {
+        $this->repository = $repository;
+    }
+}
