3852040dc045ca698ce646f083c6aedf2976898d
Samuel De Backer <sdebacker@gmail.com>
2018-09-12 17:15:31 +0200

    prettified file

--- src/resources/assets/js/admin/select-files-window.js
+++ src/resources/assets/js/admin/select-files-window.js
@@ -1,8 +1,8 @@
-$(function () {
+$(function() {
     var closeFilepicker = function() {
         $('html, body').removeClass('noscroll');
         $(this).removeClass('filepicker-modal-open');
-    }
+    };
     $('#select-files').on('click', function(event) {
         event.preventDefault();
         $('html, body').addClass('noscroll');
@@ -11,6 +11,8 @@ $(function () {
     $('#close-filepicker').on('click', function(event) {
         event.preventDefault();
         $('html, body').removeClass('noscroll');
-        $('#filepicker').removeClass('filepicker-modal-open').removeClass('filepicker-modal-no-overlay');
+        $('#filepicker')
+            .removeClass('filepicker-modal-open')
+            .removeClass('filepicker-modal-no-overlay');
     });
 });
