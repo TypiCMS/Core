11c18b5923a76cac851f93fa21fec7c34f420db0
Samuel De Backer <sdebacker@gmail.com>
2018-09-12 17:16:20 +0200

    small buttons

--- src/resources/views/admin/_buttons-form.blade.php
+++ src/resources/views/admin/_buttons-form.blade.php
@@ -1,11 +1,11 @@
 <div class="btn-toolbar">
-    <button class="btn-primary btn mr-2" value="true" id="exit" name="exit" type="submit">{{ __('Save and exit') }}</button>
-    <button class="btn-light btn mr-2" type="submit">{{ __('Save') }}</button>
+    <button class="btn btn-sm btn-primary mr-2" value="true" id="exit" name="exit" type="submit">{{ __('Save and exit') }}</button>
+    <button class="btn btn-sm btn-light mr-2" type="submit">{{ __('Save') }}</button>
     @if ($model->getTable() == 'pages' || TypiCMS::getPageLinkedToModule($model->getTable()))
-    <a class="btn btn-light btn-preview mr-2" href="{{ $model->previewUri() }}?preview=true">{{ __('Preview') }}</a>
+    <a class="btn btn-sm btn-light btn-preview mr-2" href="{{ $model->previewUri() }}?preview=true">{{ __('Preview') }}</a>
     @endif
     @if (method_exists($model, 'files'))
-    <button class="btn btn-success mr-2" id="select-files" type="button" {!! $model->id ? '' : 'disabled title="'.__('Save this item first, then add files.').'"' !!}>{{ __('Add files') }}</button>
+    <button class="btn btn-sm btn-success mr-2" id="select-files" type="button" {!! $model->id ? '' : 'disabled title="'.__('Save this item first, then add files.').'"' !!}>{{ __('Add files') }}</button>
     @endif
     {{ $slot }}
     @include('core::admin._lang-switcher-for-form')
