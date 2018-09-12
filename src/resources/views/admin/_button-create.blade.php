b0b39c8e0f72cc33c87b2dd885be49bad77ec647
Samuel De Backer <sdebacker@gmail.com>
2018-09-12 22:18:48 +0200

    included buttons hacks

--- src/resources/views/admin/_button-create.blade.php
+++ src/resources/views/admin/_button-create.blade.php
@@ -1,5 +1,5 @@
 @can ('create-'.str_singular($module))
-<a class="btn btn-primary btn-sm mr-2" href="{{ route('admin::create-'.str_singular($module)) }}">
+<a class="btn btn-primary btn-sm mr-2" href="{{ $url ?? route('admin::create-'.str_singular($module)) }}">
     <i class="fa fa-plus text-white-50"></i> @lang('Add')
 </a>
 @endcan
