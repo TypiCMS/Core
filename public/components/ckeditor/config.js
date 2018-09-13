120174175fa778da2282d20bd4ee97b61a3781ef
Samuel De Backer <sdebacker@gmail.com>
2018-09-13 08:26:00 +0200

    ckeditor config

--- public/components/ckeditor/config.js
+++ public/components/ckeditor/config.js
@@ -1,16 +1,37 @@
 CKEDITOR.dtd.$removeEmpty.span = 0;
 CKEDITOR.dtd.$removeEmpty.i = 0;
 
-CKEDITOR.editorConfig = function( config ) {
+CKEDITOR.editorConfig = function(config) {
     config.toolbar = [
-        { name: 'styles', items: [ 'Format', 'Styles', 'Font', 'FontSize' ] },
-        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
-        { name: 'align', groups: [ 'align' ], items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-        { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-        { name: 'insert', items: [ 'Image', 'oembed', 'Table', 'HorizontalRule', 'PageBreak', 'Iframe' ] },
-        { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-        { name: 'tools', items: [ 'Maximize', 'ShowBlocks', 'Source' ] },
+        { name: 'styles', items: ['Format', 'Styles', 'Font', 'FontSize'] },
+        {
+            name: 'basicstyles',
+            groups: ['basicstyles', 'cleanup'],
+            items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'],
+        },
+        {
+            name: 'paragraph',
+            groups: ['list', 'indent', 'blocks', 'bidi'],
+            items: [
+                'NumberedList',
+                'BulletedList',
+                '-',
+                'Outdent',
+                'Indent',
+                '-',
+                'Blockquote',
+                'CreateDiv',
+                '-',
+                'BidiLtr',
+                'BidiRtl',
+                'Language',
+            ],
+        },
+        { name: 'align', groups: ['align'], items: ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+        { name: 'insert', items: ['Image', 'oembed', 'Table', 'HorizontalRule', 'PageBreak', 'Iframe'] },
+        { name: 'colors', items: ['TextColor', 'BGColor'] },
+        { name: 'tools', items: ['Maximize', 'ShowBlocks', 'Source'] },
     ];
 
     // Remove some buttons provided by the standard plugins, which are
@@ -31,27 +52,23 @@ CKEDITOR.editorConfig = function( config ) {
 
     // codemirror
     config.codemirror = {
-        theme: 'twilight'
-    }
+        theme: 'twilight',
+    };
 
     // Language
     config.language = $('html').attr('lang');
 
-    // Simplify the dialog windows.
-    config.removeDialogTabs = 'image:Link;link:advanced;link:send';
-
     // File browser
     config.filebrowserBrowseUrl = '/admin/files?view=filepicker';
     config.filebrowserImageBrowseUrl = '/admin/files?type=i&view=filepicker';
-
 };
 
-CKEDITOR.on('dialogDefinition', function (event) {
+CKEDITOR.on('dialogDefinition', function(event) {
     var editor = event.editor;
     var dialogDefinition = event.data.definition;
     var dialogName = event.data.name;
 
-    var cleanUpFuncRef = CKEDITOR.tools.addFunction(function () {
+    var cleanUpFuncRef = CKEDITOR.tools.addFunction(function() {
         $('#filepicker')
             .removeClass('filepicker-single')
             .removeClass('filepicker-modal-no-overlay')
@@ -68,7 +85,7 @@ CKEDITOR.on('dialogDefinition', function (event) {
 
         if (browseButton !== null) {
             browseButton.hidden = false;
-            browseButton.onClick = function (dialog, i) {
+            browseButton.onClick = function(dialog, i) {
                 editor._.filebrowserSe = this;
                 $('#filepicker')
                     .addClass('filepicker-single')
@@ -78,8 +95,7 @@ CKEDITOR.on('dialogDefinition', function (event) {
                     .data('CKEditorCleanUpFuncNum', cleanUpFuncRef)
                     .data('CKEditorFuncNum', CKEDITOR.instances[event.editor.name]._.filebrowserFn);
                 $('html, body').addClass('noscroll');
-            }
+            };
         }
     }
 }); // dialogDefinition
-
