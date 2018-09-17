28980d0b8741ae41b25d8eea77adde04fb8c4d5f
Samuel De Backer <sdebacker@gmail.com>
2018-09-17 11:28:21 +0200

    missing type="button"

--- src/resources/js/components/ItemListStatusButton.vue
+++ src/resources/js/components/ItemListStatusButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="btn btn-xs btn-link btn-status" @click="$parent.$emit('toggle-status', model)">
+    <button class="btn btn-xs btn-link btn-status" type="button" @click="$parent.$emit('toggle-status', model)">
         <span class="fa btn-status-switch" :class="model.status_translated === '1' ? 'fa-toggle-on' : 'fa-toggle-off'"></span>
     </button>
 </template>
