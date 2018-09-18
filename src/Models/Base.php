385683b4938617ffcd51127d2e919763eda42ce4
Samuel De Backer <sdebacker@gmail.com>
2018-09-18 15:03:13 +0200

    scopeTranslated refactored

--- src/Models/Base.php
+++ src/Models/Base.php
@@ -116,9 +116,13 @@ abstract class Base extends Model
         }
     }
 
-    public function scopeTranslated($query, array $columns)
+    public function scopeTranslated($query, $columns)
     {
-        foreach ($columns as $column) {
+        $translatableColumns = [];
+        if ($columns !== null) {
+            $translatableColumns = explode(',', $columns);
+        }
+        foreach ($translatableColumns as $column) {
             $query->selectRaw('`'.$column.'`->>"$.'.request('locale', config('app.locale')).'" COLLATE utf8mb4_unicode_ci `'.$column.'_translated`');
         }
 
