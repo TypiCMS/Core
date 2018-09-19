8c71e4b833faaf4f6956b9f725fbab0e795c7b54
Samuel De Backer <sdebacker@gmail.com>
2018-09-19 13:29:00 +0200

    BaseAdminController cleaned

--- src/Http/Controllers/BaseAdminController.php
+++ src/Http/Controllers/BaseAdminController.php
@@ -2,7 +2,6 @@
 
 namespace TypiCMS\Modules\Core\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 abstract class BaseAdminController extends Controller
@@ -12,49 +11,6 @@ abstract class BaseAdminController extends Controller
     public function __construct($repository = null)
     {
         $this->repository = $repository;
-    }
-
-    /**
-     * Update the specified resources in storage.
-     *
-     * @param array   $ids
-     * @param Request $request
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function ajaxUpdate($ids, Request $request)
-    {
-        if ($request->has('remove')) {
-            return $this->repository->removeFiles($ids, $request);
-        }
-        if ($request->has('files')) {
-            return $this->repository->addFiles($ids, $request);
-        }
-
-        $data = [];
-        foreach ($request->all() as $column => $content) {
-            if (is_array($content)) {
-                foreach ($content as $key => $value) {
-                    $data[$column.'->'.$key] = $value;
-                }
-            } else {
-                $data[$column] = $content;
-            }
-        }
-
-        $number = 0;
-        foreach (explode(',', $ids) as $id) {
-            $model = $this->repository->find($id);
-            foreach ($data as $key => $value) {
-                $model->$key = $value;
-            }
-            $model->save();
-            $number += 1;
-        }
-
-        $this->repository->forgetCache();
-
-        return response()->json(compact('number'));
     }
 
     /**
