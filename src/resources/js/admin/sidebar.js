44042fe61067aecec95dbe0c633bd7aabde98639
Samuel De Backer <sdebacker@gmail.com>
2018-09-19 13:29:44 +0200

    Code style

--- src/resources/js/admin/sidebar.js
+++ src/resources/js/admin/sidebar.js
@@ -1,22 +1,20 @@
-$(function () {
-
+$(function() {
     function updatepreferences(key, value) {
         var data = {};
         data[key] = value;
         $.ajax({
             type: 'POST',
-            data: data
-        }).fail(function () {
             url: '/api/users/current/updatepreferences',
+            data: data,
+        }).fail(function() {
             alertify.error('User preference couldn’t be set.');
         });
     }
 
-    $('.panel-collapse').on('hide.bs.collapse', function () {
+    $('.panel-collapse').on('hide.bs.collapse', function() {
         updatepreferences('menus_' + $(this).attr('id') + '_collapsed', 'true');
     });
-    $('.panel-collapse').on('show.bs.collapse', function () {
+    $('.panel-collapse').on('show.bs.collapse', function() {
         updatepreferences('menus_' + $(this).attr('id') + '_collapsed', '');
     });
-
 });
