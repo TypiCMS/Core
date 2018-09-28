989638ad00c9519b5e983defee406324060fedb0
Samuel De Backer <sdebacker@gmail.com>
2018-09-28 17:30:30 +0200

    header markup

--- src/resources/views/admin/_button-back.blade.php
+++ src/resources/views/admin/_button-back.blade.php
@@ -1,1 +1,3 @@
-<a class="btn-back" href="{{ route('admin::index-'.$module) }}" title="{{ __('Back to '.$module.' list') }}"><span class="text-muted fa fa-arrow-circle-left"></span><span class="sr-only">{{ __('Back to '.$module.' list') }}</span></a>
+<a class="btn-back" href="{{ $url ?? route('admin::index-'.$module) }}" title="{{ __('Back') }}">
+    <span class="fa fa-arrow-circle-left"></span><span class="sr-only">{{ __('Back') }}</span>
+</a>
