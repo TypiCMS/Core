3adb9064b553bdfc9dba8819b54ea2bfacc74857
Samuel De Backer <sdebacker@gmail.com>
2019-01-31 16:12:11 +0100

    check if route exists

--- src/resources/views/public/_navbar-admin-link.blade.php
+++ src/resources/views/public/_navbar-admin-link.blade.php
@@ -1,6 +1,6 @@
 @if (isset($model))
 <a class="nav-link" href="{{ $model->editUrl() }}?locale={{ config('app.locale') }}">
-@elseif (isset($page) and $page->module)
+@elseif (isset($page) and $page->module and Route::has('admin::index-'.$page->module))
 <a class="nav-link" href="{{ route('admin::index-'.$page->module) }}?locale={{ config('app.locale') }}">
 @elseif (isset($page))
 <a class="nav-link" href="{{ $page->editUrl() }}?locale={{ config('app.locale') }}">
