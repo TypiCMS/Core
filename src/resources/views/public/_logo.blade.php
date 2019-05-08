29ee893914df939331b9455cdf01106999387c42
Samuel De Backer <sdebacker@gmail.com>
2019-05-08 09:37:11 +0200

    Storage::url

--- src/resources/views/public/_logo.blade.php
+++ src/resources/views/public/_logo.blade.php
@@ -1,1 +1,1 @@
-<img class="logo" src="{{ url('storage/settings/'.config('typicms.image')) }}" width="" height="{{ $height ?? 80 }}" alt="{{ TypiCMS::title() }}">
+<img class="logo" src="{{ Storage::url('settings/'.config('typicms.image')) }}" width="" height="{{ $height ?? 80 }}" alt="{{ TypiCMS::title() }}">
