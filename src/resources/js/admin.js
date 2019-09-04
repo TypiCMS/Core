381aadfc03d201c83ee5054ee532776d2f147799
Samuel De Backer <sdebacker@gmail.com>
2019-09-04 12:39:20 +0200

    meta name=csrf-token removed

--- src/resources/js/admin.js
+++ src/resources/js/admin.js
@@ -16,16 +16,6 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
- * CSRF Token
- */
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
-
-/**
  * API Token
  */
 let apiToken = document.head.querySelector('meta[name="api-token"]');
