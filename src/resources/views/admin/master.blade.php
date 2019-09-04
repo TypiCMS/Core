450523f0a3df5c885cdff6e1dc1398132aa4ff3d
Samuel De Backer <sdebacker@gmail.com>
2019-09-04 15:39:11 +0200

    missing token

--- src/resources/views/admin/master.blade.php
+++ src/resources/views/admin/master.blade.php
@@ -5,6 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="api-token" content="{{ auth()->user()->api_token ?? '' }}">
 
     <title>[admin] @yield('title') – {{ config('typicms.'.config('typicms.admin_locale').'.website_title') }}</title>
