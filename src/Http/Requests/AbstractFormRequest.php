a5b30781e82d1591fcd0222bee13b25bd8e33ebd
Samuel De Backer <sdebacker@gmail.com>
2020-03-21 13:14:32 +0100

    Validation attributes

--- src/Http/Requests/AbstractFormRequest.php
+++ src/Http/Requests/AbstractFormRequest.php
@@ -3,6 +3,7 @@
 namespace TypiCMS\Modules\Core\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
 
 abstract class AbstractFormRequest extends FormRequest
 {
@@ -14,5 +15,19 @@ abstract class AbstractFormRequest extends FormRequest
     public function authorize()
     {
         return true;
+    }
+    public function attributes()
+    {
+        return [
+            'title.*' => Str::lower(__('Title')),
+            'slug.*' => Str::lower(__('Slug')),
+            'status.*' => Str::lower(__('Published')),
+            'summary.*' => Str::lower(__('Summary')),
+            'body.*' => Str::lower(__('Body')),
+            'website.*' => Str::lower(__('Website')),
+            'url.*' => Str::lower(__('Url')),
+            'meta_keywords.*' => Str::lower(__('Meta keywords')),
+            'meta_description.*' => Str::lower(__('Meta description')),
+        ];
     }
 }
