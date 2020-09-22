d9264df83c2fa211879ce4c1323bc217ef346524
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 10:31:43 +0200

    btn toggle status

--- src/resources/js/components/ItemListStatusButton.vue
+++ src/resources/js/components/ItemListStatusButton.vue
@@ -1,10 +1,10 @@
 <template>
-    <button class="btn btn-xs btn-link btn-status" type="button" @click="$parent.$emit('toggle-status', model)">
-        <span
-            class="fa btn-status-switch"
-            :class="model.status_translated === 1 ? 'fa-toggle-on' : 'fa-toggle-off'"
-        ></span>
-    </button>
+    <button
+        class="btn-status"
+        :class="model.status_translated === 1 ? 'btn-status-on' : 'btn-status-off'"
+        type="button"
+        @click="$parent.$emit('toggle-status', model)"
+    ></button>
 </template>
 
 <script>
