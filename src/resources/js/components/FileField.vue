f2ff8fc822422a59d50c27815a7dca76610b0123
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 11:24:28 +0200

    icons

--- src/resources/js/components/FileField.vue
+++ src/resources/js/components/FileField.vue
@@ -11,7 +11,17 @@
             <div>
                 <div v-if="id !== null" class="filemanager-item-removable">
                     <button class="filemanager-item-removable-button" type="button" @click="unsetData">
-                        <span class="fa fa-times"></span>
+                        <svg
+                            width="12"
+                            height="12"
+                            viewBox="0 0 1792 1792"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"
+                            />
+                        </svg>
                     </button>
                     <div v-if="type === 'document'"><span class="fa fa-fw fa-2x fa-file-o"></span> {{ name }}</div>
                     <div class="filemanager-item-image-wrapper" v-if="type === 'image'">
--- src/resources/js/components/FilesField.vue
+++ src/resources/js/components/FilesField.vue
@@ -22,7 +22,17 @@
             >
                 <div class="filemanager-item-wrapper">
                     <button class="filemanager-item-removable-button" @click="remove(file)" type="button">
-                        <span class="fa fa-times"></span>
+                        <svg
+                            width="12"
+                            height="12"
+                            viewBox="0 0 1792 1792"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"
+                            />
+                        </svg>
                     </button>
                     <div class="filemanager-item-icon" v-if="file.type === 'i'">
                         <div class="filemanager-item-image-wrapper">
