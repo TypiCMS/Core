b934b8f1983f9ed70036299246e9e669564f35db
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 11:27:37 +0200

    Update FileManager.vue

--- src/resources/js/components/FileManager.vue
+++ src/resources/js/components/FileManager.vue
@@ -21,7 +21,17 @@
             </div>
 
             <button class="filepicker-btn-close" type="button" v-if="this.modal" @click="closeModal">
-                <span class="fa fa-close"></span>
+                <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 1792 1792"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"
+                    />
+                </svg>
             </button>
 
             <div class="btn-toolbar mb-4">
