482eb040fdc3b59e1e942afc70d7eac4e2e0f641
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 22:59:51 +0200

    Update FileManager.vue

--- src/resources/js/components/FileManager.vue
+++ src/resources/js/components/FileManager.vue
@@ -50,7 +50,7 @@
             <div class="btn-toolbar mb-4">
                 <button class="btn btn-sm btn-light mr-2" @click="newFolder(folder.id)" type="button">
                     <svg
-                        class="mr-1"
+                        class="mr-1 text-muted"
                         width="1em"
                         height="1em"
                         viewBox="0 0 16 16"
@@ -59,11 +59,7 @@
                     >
                         <path
                             fill-rule="evenodd"
-                            d="M9.828 4H2.19a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91H9v1H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181L15.546 8H14.54l.265-2.91A1 1 0 0 0 13.81 4H9.828zm-2.95-1.707L7.587 3H2.19c-.24 0-.47.042-.684.12L1.5 2.98a1 1 0 0 1 1-.98h3.672a1 1 0 0 1 .707.293z"
-                        />
-                        <path
-                            fill-rule="evenodd"
-                            d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"
+                            d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"
                         />
                     </svg>
                     {{ $t('New folder') }}
@@ -110,10 +106,10 @@
                         @click="switchView('grid')"
                     >
                         <svg
+                            class="text-muted"
                             width="1em"
                             height="1em"
                             viewBox="0 0 16 16"
-                            class="bi bi-grid-3x2-gap-fill"
                             fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
                         >
@@ -130,10 +126,10 @@
                         @click="switchView('list')"
                     >
                         <svg
+                            class="text-muted"
                             width="1em"
                             height="1em"
                             viewBox="0 0 16 16"
-                            class="bi bi-list-ul"
                             fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
                         >
