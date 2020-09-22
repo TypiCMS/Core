38ffa2669b02e63412ffefe1799af40ba55208e7
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 21:32:20 +0200

    social networks links

--- src/resources/views/public/_social-links.blade.php
+++ src/resources/views/public/_social-links.blade.php
@@ -1,28 +1,28 @@
 <div class="social-links">
     <ul class="social-links-list">
         <li class="social-links-item">
-            <a class="social-links-link social-links-link-facebook" href="https://www.facebook.com/sharer/sharer.php?u={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
-                <span class="fa fa-fw fa-facebook-official"></span>
+            <a class="social-links-link social-links-facebook" href="https://www.facebook.com/sharer/sharer.php?u={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
+                <span class="social-links-icon social-links-facebook-icon"></span>
             </a>
         </li>
         <li class="social-links-item">
-            <a class="social-links-link social-links-link-twitter" href="https://twitter.com/intent/tweet?text={{ $model->title }}&url={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
-                <span class="fa fa-fw fa-twitter"></span>
+            <a class="social-links-link social-links-twitter" href="https://twitter.com/intent/tweet?text={{ $model->title }}&url={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
+                <span class="social-links-icon social-links-twitter-icon"></span>
             </a>
         </li>
         <li class="social-links-item">
-            <a class="social-links-link social-links-link-linkedin" href="https://www.linkedin.com/sharing/share-offsite/?url={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
-                <span class="fa fa-fw fa-linkedin"></span>
+            <a class="social-links-link social-links-linkedin" href="https://www.linkedin.com/sharing/share-offsite/?url={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
+                <span class="social-links-icon social-links-linkedin-icon"></span>
             </a>
         </li>
         <li class="social-links-item">
-            <a class="social-links-link social-links-link-whatsapp" href="https://wa.me/?text={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
-                <span class="fa fa-fw fa-whatsapp"></span>
+            <a class="social-links-link social-links-whatsapp" href="https://wa.me/?text={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
+                <span class="social-links-icon social-links-whatsapp-icon"></span>
             </a>
         </li>
         <li class="social-links-item">
-            <a class="social-links-link social-links-link-mail" href="mailto:?subject={{ $model->title }}&body={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
-                <span class="fa fa-fw fa-envelope"></span>
+            <a class="social-links-link social-links-mail" href="mailto:?subject={{ $model->title }}&body={{ url()->current() }}" target="_blank" rel="noopener noreferrer">
+                <span class="social-links-icon social-links-mail-icon"></span>
             </a>
         </li>
     </ul>
