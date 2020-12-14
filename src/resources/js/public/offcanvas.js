ba9beb91666d493c6914ca50667e5676be7c1e0c
Samuel De Backer <sdebacker@gmail.com>
2020-12-14 21:37:24 +0100

    offcanvas.js without jQuery

--- src/resources/js/public/offcanvas.js
+++ src/resources/js/public/offcanvas.js
@@ -1,6 +1,14 @@
-$(function () {
-    $('[data-toggle="offcanvas"]').click(function (e) {
-        $('.site-header-offcanvas').toggleClass('active');
-        e.preventDefault();
-    });
-});
+(function () {
+    const items = document.querySelectorAll('[data-toggle="offcanvas"]');
+    for (i = 0; i < items.length; i++) {
+        items[i].addEventListener('click', function (event) {
+            const element = event.target || event.srcElement;
+            const sidebar = document.getElementById('navigation');
+            sidebar.classList.toggle('active');
+            const isOpen = sidebar.classList.contains('active');
+            for (j = 0; j < items.length; j++) {
+                items[j].setAttribute('aria-expanded', isOpen);
+            }
+        });
+    }
+})();
