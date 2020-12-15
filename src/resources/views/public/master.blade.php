450904abe439e7348dbb652f93a7ba7a177db879
Samuel De Backer <sdebacker@gmail.com>
2020-12-15 22:35:24 +0100

    Update master.blade.php

--- src/resources/views/public/master.blade.php
+++ src/resources/views/public/master.blade.php
@@ -57,7 +57,7 @@
                     </svg>
                 </a>
                 <div class="site-header-offcanvas" id="navigation">
-                    <button class="d-flex d-lg-none btn-offcanvas btn-offcanvas-close" data-toggle="offcanvas" title="@lang('Close navigation')" aria-label="@lang('Close navigation')" aria-controls="navigation" aria-expanded="false">
+                    <button class="d-flex d-lg-none btn-offcanvas btn-offcanvas-close" type="button" data-toggle="offcanvas" title="@lang('Close navigation')" aria-label="@lang('Close navigation')" aria-controls="navigation" aria-expanded="false">
                         <svg width="2.5rem" height="2.5rem" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                         </svg>
