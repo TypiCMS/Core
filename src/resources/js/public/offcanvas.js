1646acd3654b8a9136a3639db5f66897c92983db
Samuel De Backer <sdebacker@gmail.com>
2020-12-15 22:34:43 +0100

    Update offcanvas.js

--- src/resources/js/public/offcanvas.js
+++ src/resources/js/public/offcanvas.js
@@ -1,14 +1,15 @@
 (function () {
+    const sidebar = document.getElementById('navigation');
     const items = document.querySelectorAll('[data-toggle="offcanvas"]');
     for (i = 0; i < items.length; i++) {
         items[i].addEventListener('click', function (event) {
             const element = event.target || event.srcElement;
-            const sidebar = document.getElementById('navigation');
             sidebar.classList.toggle('active');
             const isOpen = sidebar.classList.contains('active');
             for (j = 0; j < items.length; j++) {
                 items[j].setAttribute('aria-expanded', isOpen);
             }
+            event.preventDefault();
         });
     }
 })();
