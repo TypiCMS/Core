e866a563e8102a382eccaba6bbffca7220a66594
Samuel De Backer <sdebacker@gmail.com>
2020-12-16 15:57:36 +0100

    Update offcanvas.js

--- src/resources/js/public/offcanvas.js
+++ src/resources/js/public/offcanvas.js
@@ -3,7 +3,6 @@
     const items = document.querySelectorAll('[data-toggle="offcanvas"]');
     for (i = 0; i < items.length; i++) {
         items[i].addEventListener('click', function (event) {
-            const element = event.target || event.srcElement;
             sidebar.classList.toggle('active');
             const isOpen = sidebar.classList.contains('active');
             for (j = 0; j < items.length; j++) {
