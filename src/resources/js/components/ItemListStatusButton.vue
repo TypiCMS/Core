cbc0680f6f2b6c30d0550e3ea9b85d5b9429fd49
Mark <markleidman@gmail.com>
2021-02-06 20:42:07 +0200

    status not translatable

--- src/resources/js/components/ItemListStatusButton.vue
+++ src/resources/js/components/ItemListStatusButton.vue
@@ -1,19 +1,28 @@
 <template>
-    <button
-        class="btn-status"
-        :class="model.status_translated === 1 ? 'btn-status-on' : 'btn-status-off'"
-        type="button"
-        @click="$parent.$emit('toggle-status', model)"
-    ></button>
+  <button
+      class="btn-status"
+      :class="statusOn ? 'btn-status-on' : 'btn-status-off'"
+      type="button"
+      @click="$parent.$emit('toggle-status', model)"
+  ></button>
 </template>
 
 <script>
 export default {
-    props: {
-        model: {
-            type: Object,
-            required: true,
-        },
+  props: {
+    model: {
+      type: Object,
+      required: true,
     },
+  },
+  computed: {
+    statusOn() {
+      if (typeof (model.status_translated) !== 'undefined') {
+        return model.status_translated === 1;
+      } else {
+        return model.status === 1;
+      }
+    },
+  },
 };
 </script>
