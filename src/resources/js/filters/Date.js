638804f403943661cd8943dbf0e17c2160800ced
Samuel De Backer <sdebacker@gmail.com>
2021-02-24 09:07:17 +0100

    vue filters

--- src/resources/js/filters/Date.js
+++ src/resources/js/filters/Date.js
@@ -1,3 +1,6 @@
 export default function (date) {
+    if (date === null) {
+        return '';
+    }
     return new Date(date).toLocaleDateString(window.TypiCMS.locale);
 }
--- src/resources/js/filters/Datetime.js
+++ src/resources/js/filters/Datetime.js
@@ -1,3 +1,6 @@
 export default function (datetime) {
+    if (datetime === null) {
+        return '';
+    }
     return new Date(datetime).toLocaleString(window.TypiCMS.locale);
 }
