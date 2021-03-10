91317c000b9abbacff76b0ee04d156df9dc2691a
Samuel De Backer <sdebacker@gmail.com>
2021-03-10 15:02:32 +0100

    ===

--- src/resources/views/admin/_buttons-form.blade.php
+++ src/resources/views/admin/_buttons-form.blade.php
@@ -1,7 +1,7 @@
 <div class="btn-toolbar mb-4">
     <button class="btn btn-sm btn-primary me-2" value="true" id="exit" name="exit" type="submit">{{ __('Save and exit') }}</button>
     <button class="btn btn-sm btn-light me-2" type="submit">{{ __('Save') }}</button>
-    @if ($model->getTable() == 'pages' || Route::has($locale.'::'.Str::singular($model->getTable())))
+    @if ($model->getTable() === 'pages' || Route::has($locale.'::'.Str::singular($model->getTable())))
     <a class="btn btn-sm btn-light btn-preview me-2" href="{{ $model->previewUri() }}?preview=true">{{ __('Preview') }}</a>
     @endif
     {{ $slot }}
