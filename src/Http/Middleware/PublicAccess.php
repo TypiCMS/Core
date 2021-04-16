4dcd83ef6711d6ccd62deba9e3409123d6921991
Samuel De Backer <sdebacker@gmail.com>
2021-04-16 13:35:30 +0200

    Missing use statement

--- src/Http/Middleware/PublicAccess.php
+++ src/Http/Middleware/PublicAccess.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Route;
 
 class PublicAccess
 {
