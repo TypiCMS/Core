6c893f55edf37f99f321a79538b4f6ba86536ba9
Samuel De Backer <sdebacker@gmail.com>
2021-09-28 09:02:04 +0200

    Translations

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -12,9 +12,9 @@ return [
     |
     */
 
-    'password' => 'Les mots de passe doivent contenir au moins six caractères et doivent être identiques.',
     'reset' => 'Votre mot de passe a été réinitialisé !',
-    'sent' => 'Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe !',
+    'sent' => 'Nous vous avons envoyé par email le lien de réinitialisation du mot de passe !',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
     'token' => 'Ce jeton de réinitialisation du mot de passe n’est pas valide.',
-    'user' => 'Aucun utilisateur n’a été trouvé avec cette adresse e-mail.',
+    'user' => 'Aucun utilisateur n’a été trouvé avec cette adresse email.',
 ];
