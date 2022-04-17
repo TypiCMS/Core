ac0ffced0d9445155c8a009ba25ba0d9f91a3b0d
Samuel De Backer <sdebacker@gmail.com>
2022-04-17 16:53:05 +0200

    Seeders

---
+++ database/seeders/MenuSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class MenuSeeder extends Seeder
+{
+    public function run()
+    {
+        $typi_menus = [
+            ['id' => 1, 'image_id' => null, 'name' => 'main', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 2, 'image_id' => null, 'name' => 'footer', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 3, 'image_id' => null, 'name' => 'social', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 4, 'image_id' => null, 'name' => 'legal', 'class' => null, 'status' => '{"fr": 1, "en": 1, "nl": 1}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ];
+
+        DB::table('menus')->insert($typi_menus);
+    }
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    public function run()
+    {
+        $typi_roles = [
+            ['id' => 1, 'name' => 'administrator', 'guard_name' => 'web', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 2, 'name' => 'visitor', 'guard_name' => 'web', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ];
+
+        DB::table('roles')->insert($typi_roles);
+    }
+}
