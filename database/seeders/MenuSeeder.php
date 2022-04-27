ff3ae79092ec83aa39f3914c3b1b2452b53884a3
Samuel De Backer <sdebacker@gmail.com>
2022-04-27 16:20:02 +0200

    Update MenuSeeder.php

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -11,7 +11,7 @@ class MenuSeeder extends Seeder
     public function run()
     {
         $menus = [
-            ['id' => 1, 'image_id' => null, 'name' => 'main', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['id' => 1, 'image_id' => null, 'name' => 'primary', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['id' => 2, 'image_id' => null, 'name' => 'footer', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['id' => 3, 'image_id' => null, 'name' => 'social', 'class' => null, 'status' => '{"fr":"1","en":"1","nl":"1"}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
             ['id' => 4, 'image_id' => null, 'name' => 'legal', 'class' => null, 'status' => '{"fr": 1, "en": 1, "nl": 1}', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
