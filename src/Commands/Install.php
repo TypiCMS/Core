ae87abd844667da15d9a1795ac4aff2e89024284
Samuel De Backer <sdebacker@gmail.com>
2022-11-16 17:18:12 +0100

    Code style

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -2,7 +2,6 @@
 
 namespace TypiCMS\Modules\Core\Commands;
 
-use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 use TypiCMS\Modules\Core\Providers\ModuleServiceProvider;
@@ -94,7 +93,7 @@ class Install extends Command
             $name = explode('.', $segments[1])[0];
 
             return Str::slug($name);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return '';
         }
     }
