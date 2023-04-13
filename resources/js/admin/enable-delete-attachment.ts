7dc2464a07e2ee57cad71499d167d4af19fd076b
Samuel De Backer <sdebacker@gmail.com>
2023-04-13 10:12:38 +0200

    Update enable-delete-attachment.ts

--- resources/js/admin/enable-delete-attachment.ts
+++ resources/js/admin/enable-delete-attachment.ts
@@ -3,36 +3,38 @@ import alertify from 'alertify.js';
 
 type DeleteButton = HTMLElement | null;
 
+const handleDeleteAttachment = async (attachment: DeleteButton): Promise<void> => {
+    const field = attachment?.dataset?.field;
+
+    if (!field) {
+        return;
+    }
+
+    if (!confirm(`Delete ${field}?`)) {
+        return;
+    }
+
+    try {
+        const response = await fetcher('/admin/settings', {
+            method: 'PATCH',
+            body: JSON.stringify({ [field]: 'delete' }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to delete attachment. Network response was not ok.');
+        }
+
+        document.querySelector('.fieldset-preview')?.remove();
+    } catch (error) {
+        alertify.error('An error occurred while deleting attachment.');
+        console.error('There was a problem with the fetch operation:', error);
+    }
+};
+
 export default (): void => {
     const attachmentElements: NodeListOf<DeleteButton> = document.querySelectorAll('.delete-attachment');
 
-    attachmentElements.forEach((attachment: DeleteButton) => {
-        const field: string | undefined = attachment?.dataset?.field;
-
-        if (field) {
-            if (attachment) {
-                attachment.addEventListener('click', async () => {
-                    if (!confirm(`Delete ${field}?`)) {
-                        return false;
-                    }
-
-                    try {
-                        const response = await fetcher('/admin/settings', {
-                            method: 'PATCH',
-                            body: JSON.stringify({ [field]: 'delete' }),
-                        });
-
-                        if (!response.ok) {
-                            throw new Error('Failed to delete attachment. Network response was not ok.');
-                        }
-
-                        document.querySelector('.fieldset-preview')?.remove();
-                    } catch (error) {
-                        alertify.error('An error occurred while deleting attachment.');
-                        console.error('There was a problem with the fetch operation:', error);
-                    }
-                });
-            }
-        }
+    attachmentElements.forEach((attachment) => {
+        attachment?.addEventListener('click', () => handleDeleteAttachment(attachment));
     });
 };
