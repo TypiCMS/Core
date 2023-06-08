02e7fc51393803951ffe4acc39cf894b7859c0e7
Samuel De Backer <sdebacker@gmail.com>
2023-06-08 11:30:14 +0200

    document-list

--- resources/views/files/public/_document-list-item.blade.php
+++ resources/views/files/public/_document-list-item.blade.php
@@ -1,12 +1,11 @@
 <li class="document-list-item">
     <a class="document-list-item-link" href="{{ Storage::url($document->path) }}" target="_blank" rel="noopener noreferrer">
-        <i class="document-list-item-icon bi bi-file-earmark-fill"></i>
+        <i class="document-list-item-icon bi bi-file-earmark-arrow-down"></i>
         <div class="document-list-item-info">
             <span class="document-list-item-filename">{{ $document->present()->title }}</span>
-            <div class="document-list-item-meta">
-                <small class="document-list-item-meta-extension">{{ $document->extension }}</small>
-                <small class="document-list-item-meta-filesize">{{ $document->present()->filesize }}</small>
-            </div>
+            <small class="document-list-item-meta">
+                ({{ mb_strtoupper($document->extension) }}, {{ $document->present()->filesize }})
+            </small>
         </div>
     </a>
 </li>
