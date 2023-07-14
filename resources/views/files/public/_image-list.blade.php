442408223545e9cecc21be584aab091899fd1d03
Samuel De Backer <sdebacker@gmail.com>
2023-07-14 17:45:09 +0200

    photoswipe

--- resources/views/files/public/_image-list.blade.php
+++ resources/views/files/public/_image-list.blade.php
@@ -1,7 +1,20 @@
 @if ($model->images->count() > 0)
-    <ul class="image-list-list">
+    <ul class="image-list-list" id="image-list-list">
         @foreach ($model->images as $image)
             @include('files::public._image-list-item')
         @endforeach
     </ul>
 @endif
+
+@push('js')
+    <script type="module">
+        import PhotoSwipeLightbox from '/components/photoswipe/photoswipe-lightbox.esm.min.js';
+
+        const lightbox = new PhotoSwipeLightbox({
+            gallery: '#image-list-list',
+            children: 'a',
+            pswpModule: () => import('/components/photoswipe/photoswipe.esm.min.js'),
+        });
+        lightbox.init();
+    </script>
+@endpush
