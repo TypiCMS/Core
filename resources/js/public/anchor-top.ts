7ec4f8c73e197a4bd1b25d920b378257ba4a7445
Samuel De Backer <sdebacker@gmail.com>
2023-09-21 21:07:47 +0200

    Some typescript code style

--- resources/js/public/anchor-top.ts
+++ resources/js/public/anchor-top.ts
@@ -1,6 +1,6 @@
 export default (): void => {
     window.addEventListener('scroll', (): void => {
-        const anchorTop = document.getElementById('anchor-top') as HTMLElement;
+        const anchorTop: HTMLAnchorElement = document.getElementById('anchor-top') as HTMLAnchorElement;
         if (window.scrollY > 300) {
             anchorTop.classList.remove('disabled');
         } else {
--- resources/js/public/offcanvas.ts
+++ resources/js/public/offcanvas.ts
@@ -1,13 +1,11 @@
-export default () => {
+export default (): void => {
     const navigation = document.getElementById('navigation') as HTMLElement;
     const items = document.querySelectorAll('[data-toggle="offcanvas"]');
 
     items.forEach((item) => {
         item.addEventListener('click', (event) => {
             navigation.classList.toggle('active');
-            const isOpen: string = navigation.classList.contains('active')
-                ? 'true'
-                : 'false';
+            const isOpen: string = navigation.classList.contains('active') ? 'true' : 'false';
 
             items.forEach((item) => {
                 item.setAttribute('aria-expanded', isOpen);
