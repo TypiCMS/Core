58cd26571fedae5fef9cfc892717cadae766d150
Samuel De Backer <sdebacker@gmail.com>
2023-09-29 20:11:13 +0200

    Update config-light.js

--- public/components/ckeditor4/config-light.js
+++ public/components/ckeditor4/config-light.js
@@ -1,16 +1,13 @@
-var editors = document.querySelectorAll('.ckeditor-light');
-for (var i = 0; i < editors.length; ++i) {
+const editors = document.querySelectorAll('.ckeditor-light');
+const publicCssFile = window.TypiCMS.public_css_file;
+
+for (let i = 0; i < editors.length; ++i) {
     CKEDITOR.replace(editors[i].id, {
-        toolbar: [
-            { items: ['Bold', 'Italic', 'Subscript', 'Superscript'] },
-            { items: ['RemoveFormat'] },
-            { items: ['Link', 'Unlink'] },
-            { items: ['Source'] },
-        ],
+        toolbar: [{ items: ['Bold', 'Italic', 'Subscript', 'Superscript'] }, { items: ['RemoveFormat'] }, { items: ['Link', 'Unlink'] }, { items: ['Source'] }],
         entities: false,
         emailProtection: 'encode',
         height: 240,
-        contentsCss: ['/css/public.css', '/components/ckeditor4/custom.css'],
+        contentsCss: [publicCssFile, '/components/ckeditor4/custom.css'],
         language: document.documentElement.lang,
         stylesSet: [],
         extraPlugins: 'adv_link,codemirror',
