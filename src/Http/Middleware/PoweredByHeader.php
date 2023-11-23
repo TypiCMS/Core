fb465efac6e0b00f449cc5223d5d94128e3f8ecd
Samuel De Backer <sdebacker@gmail.com>
2023-11-23 14:24:54 +0100

    Create PoweredByHeader.php

---
+++ src/Http/Middleware/PoweredByHeader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TypiCMS\Modules\Core\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class PoweredByHeader
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if (config('typicms.send_powered_by_header') && $response instanceof Response) {
+            $response->header('X-Powered-By', 'TypiCMS');
+        }
+
+        return $response;
+    }
+}
