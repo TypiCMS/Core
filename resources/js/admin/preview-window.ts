d1339556f29964d419524c2e6da3f3d5701cf563
Samuel De Backer <sdebacker@gmail.com>
2024-02-27 15:00:38 +0100

    Update preview-window.ts

--- resources/js/admin/preview-window.ts
+++ resources/js/admin/preview-window.ts
@@ -1,47 +1,33 @@
-export default () => {
+export default (): void => {
     document.body.insertAdjacentHTML(
         'beforeend',
         `<div id='preview-window' class='typicms-modal'>
             <div id='preview-window-wrapper' class='typicms-modal-wrapper'>
                 <iframe id='preview-content' class='typicms-modal-content'></iframe>
             </div>
-            <button id='close-preview' class='typicms-modal-btn-close'>
-                <i class='bi bi-x fs-3'></i>
+            <button id='close-preview' class='typicms-modal-btn-close' aria-label='Close window'>
+                <span aria-hidden="true">×</span>
             </button>
         </div>`,
     );
 
-    /**
-     * Open preview window
-     */
-    const previewButtons: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('.btn-preview');
-    previewButtons.forEach((button: HTMLAnchorElement): void => {
-        button.addEventListener(
-            'click',
-            (event: Event) => {
-                event.preventDefault();
-                (document.getElementById('preview-content') as HTMLIFrameElement).src = button.href;
-                document.body.classList.add('noscroll'); // add noscroll class to <body>
-                document.getElementById('preview-window')!.classList.add('typicms-modal-open');
-            },
-            false,
-        );
-    });
+    // Open preview window
+    document.querySelectorAll('.btn-preview').forEach((button: Element): void => button.addEventListener('click', openPreview));
 
-    /**
-     * Close preview window
-     */
-    const btnClosePreview = document.getElementById('close-preview') as HTMLButtonElement | null;
-    if (btnClosePreview) {
-        btnClosePreview.addEventListener(
-            'click',
-            (event: Event) => {
-                event.preventDefault();
-                (document.getElementById('preview-content') as HTMLIFrameElement).src = '';
-                document.body.classList.remove('noscroll'); // remove noscroll class from <body>
-                document.getElementById('preview-window')!.classList.remove('typicms-modal-open');
-            },
-            false,
-        );
+    // Close preview window
+    document.getElementById('close-preview')?.addEventListener('click', closePreview);
+    document.addEventListener('keydown', (event: KeyboardEvent) => event.code === 'Escape' && closePreview());
+
+    function openPreview(event: Event): void {
+        event.preventDefault();
+        (document.getElementById('preview-content') as HTMLIFrameElement).src = (event.target as HTMLAnchorElement).href;
+        document.body.classList.add('noscroll'); // add noscroll class to <body>
+        document.getElementById('preview-window')!.classList.add('typicms-modal-open');
+    }
+
+    function closePreview(): void {
+        (document.getElementById('preview-content') as HTMLIFrameElement).src = '';
+        document.body.classList.remove('noscroll'); // remove noscroll class from <body>
+        document.getElementById('preview-window')!.classList.remove('typicms-modal-open');
     }
 };
