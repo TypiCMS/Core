c6a393969357854d0b84b657ef33ff28487db1ba
Samuel De Backer <sdebacker@gmail.com>
2024-03-27 12:28:46 +0100

    search form

--- resources/views/search/public/_form.blade.php
+++ resources/views/search/public/_form.blade.php
@@ -1,7 +1,7 @@
 @if (Route::has($lang . '::search'))
     <form method="get" action="{{ route($lang . '::search') }}" class="search-form">
         <div class="input-group">
-            <input class="search-input form-control" type="text" name="query" id="query" aria-label="@lang('Search')" placeholder="@lang('Search')" value="{{ old('query') }}" />
+            <input class="search-input form-control" type="text" name="query" id="query" aria-label="@lang('Search')" placeholder="@lang('Search')" value="{{ request('query') }}" />
             <button class="search-button btn btn-primary" type="submit">Search</button>
         </div>
     </form>
