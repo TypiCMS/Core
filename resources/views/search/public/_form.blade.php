cb763b935fb0ab4d8277f122248f31d45a3aa457
Samuel De Backer <sdebacker@gmail.com>
2024-03-27 12:25:24 +0100

    Search param changed to "query"

--- resources/views/search/public/_form.blade.php
+++ resources/views/search/public/_form.blade.php
@@ -1,7 +1,7 @@
 @if (Route::has($lang . '::search'))
     <form method="get" action="{{ route($lang . '::search') }}" class="search-form">
         <div class="input-group">
-            <input class="search-input form-control" type="text" name="search" id="search" aria-label="@lang('Search')" placeholder="@lang('Search')" value="{{ old('search') }}" />
+            <input class="search-input form-control" type="text" name="query" id="query" aria-label="@lang('Search')" placeholder="@lang('Search')" value="{{ old('query') }}" />
             <button class="search-button btn btn-primary" type="submit">Search</button>
         </div>
     </form>
