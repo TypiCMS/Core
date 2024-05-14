82738997595e1532f37225d4ba556b84b3ed8ac9
Samuel De Backer <sdebacker@gmail.com>
2024-05-14 22:53:18 +0200

    Images moved from public dir.

--- resources/views/core/public/_logo.blade.php
+++ resources/views/core/public/_logo.blade.php
@@ -1,1 +1,1 @@
-<img class="{{ $class }}" src="{{ url(config('typicms.logo')) }}" alt="{{ TypiCMS::title() }}" width="128" height="96" />
+<img class="{{ $class }}" src="{{ Vite::asset(config('typicms.logo')) }}" alt="{{ TypiCMS::title() }}" width="128" height="96"/>
