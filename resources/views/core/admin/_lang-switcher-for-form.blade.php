446bcb2d7a271055003969b89257fcf3d17aa79b
Samuel De Backer <sdebacker@gmail.com>
2024-05-27 17:18:36 +0200

    Update _lang-switcher-for-form.blade.php

--- resources/views/core/admin/_lang-switcher-for-form.blade.php
+++ resources/views/core/admin/_lang-switcher-for-form.blade.php
@@ -1,12 +1,12 @@
 @if (count($locales) > 1)
     <div class="btn-group btn-group-sm ms-auto">
         <button class="btn btn-light dropdown-toggle" type="button" id="dropdownLangSwitcher" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span id="active-locale">{{ $locale ?: __('All languages') }}</span>
+            <span id="active-locale">{{ config('typicms.content_locale') ?: __('All languages') }}</span>
         </button>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownLangSwitcher">
-            @foreach ($locales as $lang)
-                <a class="dropdown-item btn-lang-js @if (!session('allLocalesInForm') && $lang == $locale) active @endif" href="#" data-locale="{{ $lang }}">
-                    @lang('languages.' . $lang)
+            @foreach ($locales as $locale)
+                <a class="dropdown-item btn-lang-js @if (!session('allLocalesInForm') && $locale === config('typicms.content_locale')) active @endif" href="#" data-locale="{{ $locale }}">
+                    @lang('languages.' . $locale)
                 </a>
             @endforeach
 
