edce066a1f99bb6f6642a3d240695f8497574d79
Samuel De Backer <sdebacker@gmail.com>
2024-10-21 12:43:07 +0200

    Default configuration form revamped

--- resources/views/settings/admin/index.blade.php
+++ resources/views/settings/admin/index.blade.php
@@ -19,92 +19,52 @@
     </div>
 
     <div class="content">
-        <div class="row">
-            <div class="col-lg-7">
-                <label class="form-label">{{ __('Website title') }}</label>
-                @foreach ($locales as $locale)
-                    <div class="mb-3">
-                        <div class="input-group">
-                            <span class="input-group-text">{{ strtoupper($locale) }}</span>
-                            <input class="form-control" type="text" name="{{ $locale }}[website_title]" value="{{ $data->$locale->website_title ?? '' }}" />
-                        </div>
-                    </div>
-                @endforeach
-
-                <label class="form-label">{{ __('Publish website') }}</label>
-
-                <div class="mb-3">
-                    @foreach ($locales as $locale)
-                        <div class="form-check form-check-inline">
-                            <input type="hidden" name="{{ $locale }}[status]" value="0" />
-                            <input class="form-check-input" type="checkbox" name="{{ $locale }}[status]" id="{{ $locale }}[status]" value="1" @if (isset($data->$locale) and $data->$locale->status) checked @endif />
-                            <label class="form-check-label" for="{{ $locale }}[status]">{{ strtoupper($locale) }}</label>
-                        </div>
-                    @endforeach
+        <label class="form-label">{{ __('Website title') }}</label>
+        @foreach ($locales as $locale)
+            <div class="mb-3">
+                <div class="input-group">
+                    <span class="input-group-text">{{ strtoupper($locale) }}</span>
+                    <input class="form-control" type="text" name="{{ $locale }}[website_title]" value="{{ $data->$locale->website_title ?? '' }}" />
                 </div>
-
-                <label class="form-label">{{ __('Website baseline') }}</label>
-                @foreach ($locales as $locale)
-                    <div class="mb-3">
-                        <div class="input-group">
-                            <span class="input-group-text">{{ strtoupper($locale) }}</span>
-                            <input class="form-control" type="text" name="{{ $locale }}[website_baseline]" value="{{ $data->$locale->website_baseline ?? '' }}" />
-                        </div>
-                    </div>
-                @endforeach
-
-                @if (!config('typicms.welcome_message_url'))
-                    {!! BootForm::textarea(__('Administration Welcome Message'), 'welcome_message')->rows(3) !!}
-                @endif
-
-                {!! BootForm::hidden('auth_public')->value(0) !!}
-                {!! BootForm::checkbox(__('Authenticate to view website'), 'auth_public') !!}
-                {!! BootForm::hidden('register')->value(0) !!}
-                {!! BootForm::checkbox(__('Registration allowed'), 'register') !!}
             </div>
+        @endforeach
 
-            <div class="col-lg-5">
-                <table class="table table-sm">
-                    <tbody>
-                    <tr>
-                        <th class="text-nowrap">
-                            <small>@lang('Environment')</small>
-                        </th>
-                        <td><small>{{ App::environment() }}</small></td>
-                    </tr>
-                    <tr>
-                        <th class="text-nowrap">
-                            <small>@lang('System locales')</small>
-                        </th>
-                        <td>
-                            <div class="container-system-locales">
-                                <small>
-                                    <?php
-                                    try {
-                                        system('locale -a');
-                                    } catch (Exception $e) {
-                                        echo $e->getMessage();
-                                    } ?>
-                                </small>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="text-nowrap">
-                            <small>@lang('Active locale')</small>
-                        </th>
-                        <td><small>{{ config('app.locale') }}</small></td>
-                    </tr>
-                    <tr>
-                        <th class="text-nowrap">
-                            <small>@lang('Cache')</small>
-                        </th>
-                        <td>
-                            <small>{{ config('laravel-model-caching.enabled') ? __('Yes') : __('No') }}</small>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+        <label class="form-label">{{ __('Publish website') }}</label>
+
+        <div class="mb-3">
+            @foreach ($locales as $locale)
+                <div class="form-check form-check-inline">
+                    <input type="hidden" name="{{ $locale }}[status]" value="0" />
+                    <input class="form-check-input" type="checkbox" name="{{ $locale }}[status]" id="{{ $locale }}[status]" value="1" @if (isset($data->$locale) and $data->$locale->status) checked @endif />
+                    <label class="form-check-label" for="{{ $locale }}[status]">{{ strtoupper($locale) }}</label>
+                </div>
+            @endforeach
+        </div>
+
+        <label class="form-label">{{ __('Website baseline') }}</label>
+        @foreach ($locales as $locale)
+            <div class="mb-3">
+                <div class="input-group">
+                    <span class="input-group-text">{{ strtoupper($locale) }}</span>
+                    <input class="form-control" type="text" name="{{ $locale }}[website_baseline]" value="{{ $data->$locale->website_baseline ?? '' }}" />
+                </div>
+            </div>
+        @endforeach
+
+        @if (!config('typicms.welcome_message_url'))
+            {!! BootForm::textarea(__('Administration Welcome Message'), 'welcome_message')->rows(3) !!}
+        @endif
+
+        {!! BootForm::hidden('auth_public')->value(0) !!}
+        {!! BootForm::hidden('register')->value(0) !!}
+
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>@lang('Contact')</h2>
+                {!! BootForm::text(__('Name'), 'contact_1_name') !!}
+                {!! BootForm::text(__('Phone'), 'contact_1_phone') !!}
+                {!! BootForm::text(__('Address'), 'contact_1_address') !!}
+                {!! BootForm::text(__('Email'), 'contact_1_email_address') !!}
             </div>
         </div>
     </div>
