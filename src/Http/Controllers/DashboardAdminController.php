662e5f7ad2d34b1e310568a8b9d170cb810745ae
Samuel De Backer <sdebacker@gmail.com>
2025-06-23 12:49:42 +0200

    Update DashboardAdminController.php

--- src/Http/Controllers/DashboardAdminController.php
+++ src/Http/Controllers/DashboardAdminController.php
@@ -2,8 +2,8 @@
 
 namespace TypiCMS\Modules\Core\Http\Controllers;
 
-use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
@@ -17,15 +17,16 @@ class DashboardAdminController extends BaseAdminController
     public function dashboard(Client $client): View
     {
         $welcomeMessage = config('typicms.welcome_message');
-        $url = config('typicms.welcome_message_url');
-
-        try {
-            $response = $client->get($url, ['timeout' => 2]);
-            if ($response->getStatusCode() < 400) {
-                $welcomeMessage = $response->getBody();
+        $url = (string) config('typicms.welcome_message_url');
+        if (!empty($url)) {
+            try {
+                $response = $client->get($url, ['timeout' => 2]);
+                if ($response->getStatusCode() < 400) {
+                    $welcomeMessage = $response->getBody();
+                }
+            } catch (GuzzleException $exception) {
+                info($exception->getMessage());
             }
-        } catch (Exception $exception) {
-            info($exception->getMessage());
         }
 
         return view('dashboard::show')->with(compact('welcomeMessage'));
