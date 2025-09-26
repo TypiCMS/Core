24f3c3ca58df71ed7b21ba4ec411cef9cde4bbe1
Samuel De Backer <sdebacker@gmail.com>
2025-09-26 15:59:07 +0200

    Code cleaned

--- resources/js/public/anchor-top.ts
+++ resources/js/public/anchor-top.ts
@@ -1,10 +1,12 @@
 export default (): void => {
     window.addEventListener('scroll', (): void => {
-        const anchorTop: HTMLDivElement = document.getElementById('anchor-top') as HTMLDivElement;
-        if (window.scrollY > 300) {
-            anchorTop.classList.remove('disabled');
-        } else {
-            anchorTop.classList.add('disabled');
+        const anchorTop = document.getElementById('anchor-top');
+        if (anchorTop) {
+            if (window.scrollY > 300) {
+                anchorTop.classList.remove('disabled');
+            } else {
+                anchorTop.classList.add('disabled');
+            }
         }
     });
 };
