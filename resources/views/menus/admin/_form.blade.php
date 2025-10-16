b47d539c3eabb4fb401e20a7aaab8aadf21999c7
Samuel De Backer <sdebacker@gmail.com>
2025-10-16 13:05:54 +0200

    forms

--- resources/views/menus/admin/_form.blade.php
+++ resources/views/menus/admin/_form.blade.php
@@ -7,26 +7,34 @@
 <div class="content">
     <x-core::form-errors />
 
-    <file-manager></file-manager>
-    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+        <div class="row">
+            <div class="col-lg-8">
+                @if ($model->id)
+                    {!! BootForm::hidden('name') !!}
+                @else
+                    {!! BootForm::text(__('Name'), 'name')->required()->autocomplete('off') !!}
+                @endif
 
-    @if ($model->id)
-        {!! BootForm::hidden('name') !!}
-    @else
-        {!! BootForm::text(__('Name'), 'name')->required()->autocomplete('off') !!}
-    @endif
+                {!! BootForm::text(__('Class'), 'class') !!}
+                <div class="mb-3">
+                    {!! TranslatableBootForm::hidden('status')->value(0) !!}
+                    {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+                </div>
 
-    {!! BootForm::text(__('Class'), 'class') !!}
-    <div class="mb-3">
-        {!! TranslatableBootForm::hidden('status')->value(0) !!}
-        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-    </div>
+                @if ($model->id)
+                    <item-list-tree url-base="/api/menus/{{ $model->id }}/menulinks" fields="id,menu_id,page_id,position,parent_id,status,title,website" table="menulinks" title="menulinks" v-if="$can('read menulinks')" :sub-list="true">
+                        <template #top-buttons v-if="$can('create menulinks')">
+                            <x-core::create-button :url="route('admin::create-menulink', $model->id)" :label="__('Create menulink')" />
+                        </template>
+                    </item-list-tree>
+                @endif
+            </div>
+            <div class="col-lg-4">
+                <div class="right-column">
+                    <file-manager></file-manager>
+                    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+                </div>
+            </div>
+        </div>
 
-    @if ($model->id)
-        <item-list-tree url-base="/api/menus/{{ $model->id }}/menulinks" fields="id,menu_id,page_id,position,parent_id,status,title,website" table="menulinks" title="menulinks" v-if="$can('read menulinks')" :sub-list="true">
-            <template #top-buttons v-if="$can('create menulinks')">
-                <x-core::create-button :url="route('admin::create-menulink', $model->id)" :label="__('Create menulink')" />
-            </template>
-        </item-list-tree>
-    @endif
 </div>
