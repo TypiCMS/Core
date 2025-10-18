cf08f76b6c10ef063b806552f8038f695e1057ac
Samuel De Backer <sdebacker@gmail.com>
2025-10-18 23:03:04 +0200

    Update _lang-switcher.blade.php

--- resources/views/core/public/_lang-switcher.blade.php
+++ resources/views/core/public/_lang-switcher.blade.php
@@ -6,22 +6,16 @@
         <ul class="lang-switcher-list dropdown-menu" aria-labelledby="dropdownLangSwitcher">
             @foreach ($enabledLocales as $locale)
                 @if ($locale !== $lang)
+                    @php
+                        $url = url('/' . $locale);
+                        if (isset($page) && $page->isPublished($locale)) {
+                            $url = isset($model) && $model->isPublished($locale) ? $model->url($locale) : $page->url($locale);
+                        }
+                    @endphp
                     <li>
-                        @isset($page)
-                            @if ($page->isPublished($locale))
-                                <a class="lang-switcher-item dropdown-item" href="{{ isset($model) && $model->isPublished($locale) ? $model->url($locale) : $page->url($locale) }}">
-                                    {{ $locale }}
-                                </a>
-                            @else
-                                <a class="lang-switcher-item dropdown-item" href="{{ url('/' . $locale) }}">
-                                    {{ $locale }}
-                                </a>
-                            @endif
-                        @else
-                            <a class="lang-switcher-item dropdown-item" href="{{ url('/' . $locale) }}">
-                                {{ $locale }}
-                            </a>
-                        @endisset
+                        <a class="lang-switcher-item" href="{{ $url }}" hreflang="{{ $locale }}">
+                            <abbr lang="{{ $locale }}" title="@lang('languages.' . $locale, [], $locale)">{{ $locale }}</abbr>
+                        </a>
                     </li>
                 @endif
             @endforeach
