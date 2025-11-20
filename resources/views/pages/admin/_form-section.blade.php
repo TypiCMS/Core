6d88bb251c65540580c6e67273f12dd8fead731f
Samuel De Backer <sdebacker@gmail.com>
2025-11-20 10:31:58 +0100

    Section form on two columns

--- resources/views/pages/admin/_form-section.blade.php
+++ resources/views/pages/admin/_form-section.blade.php
@@ -7,26 +7,33 @@
 <div class="content">
     <x-core::form-errors />
 
-    {!! BootForm::hidden('id') !!}
-    {!! BootForm::hidden('page_id')->value($page->id) !!}
-
-    <file-manager></file-manager>
-    <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
-    <files-field :init-files="{{ $model->files }}"></files-field>
-
-    <x-core::title-and-slug-fields :locales="locales()" />
-    <div class="mb-3">
-        {!! TranslatableBootForm::hidden('status')->value(0) !!}
-        {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
-    </div>
-    <div class="mb-3">
-        {!! BootForm::hidden('hide_title')->value(0) !!}
-        {!! BootForm::checkbox(__('No title'), 'hide_title') !!}
-    </div>
     <div class="row">
-        <div class="col-md-6">
-            {!! BootForm::select(__('Template'), 'template', pageSectionTemplates()) !!}
+        <div class="col-lg-8">
+            {!! BootForm::hidden('id') !!}
+            {!! BootForm::hidden('page_id')->value($page->id) !!}
+
+            <x-core::title-and-slug-fields :locales="locales()" />
+            <div class="mb-3">
+                {!! TranslatableBootForm::hidden('status')->value(0) !!}
+                {!! TranslatableBootForm::checkbox(__('Published'), 'status') !!}
+            </div>
+            <div class="mb-3">
+                {!! BootForm::hidden('hide_title')->value(0) !!}
+                {!! BootForm::checkbox(__('No title'), 'hide_title') !!}
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    {!! BootForm::select(__('Template'), 'template', pageSectionTemplates()) !!}
+                </div>
+            </div>
+            <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
+        </div>
+        <div class="col-lg-4">
+            <div class="right-column">
+                <file-manager></file-manager>
+                <file-field type="image" field="image_id" :init-file="{{ $model->image ?? 'null' }}"></file-field>
+                <files-field :init-files="{{ $model->files }}"></files-field>
+            </div>
         </div>
     </div>
-    <x-core::tiptap-editors :model="$model" name="body" :label="__('Body')" />
 </div>
