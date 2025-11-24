7be54e71f2b775171f53c102e1910378cb2e6105
Samuel De Backer <sdebacker@gmail.com>
2025-11-24 11:11:45 +0100

    Make sidebar labels and groups configurable

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -16,7 +16,7 @@ class SidebarViewComposer
                 $group->id = 'dashboard';
                 $group->weight = 10;
                 $group->hideHeading();
-                $group->addItem(__('Dashboard'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.dashboard.sidebar.label', 'Dashboard')), function (SidebarItem $item): void {
                     $item->id = 'dashboard';
                     $item->icon = config('typicms.modules.dashboard.sidebar.icon');
                     $item->weight = config('typicms.modules.dashboard.sidebar.weight');
@@ -25,10 +25,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read pages')) {
-            $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.pages.sidebar.group', 'Content')), function (SidebarGroup $group): void {
                 $group->id = 'content';
                 $group->weight = 30;
-                $group->addItem(__('Pages'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.pages.sidebar.label', 'Pages')), function (SidebarItem $item): void {
                     $item->id = 'pages';
                     $item->icon = config('typicms.modules.pages.sidebar.icon');
                     $item->weight = config('typicms.modules.pages.sidebar.weight');
@@ -37,10 +37,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read menus')) {
-            $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.menus.sidebar.group', 'Content')), function (SidebarGroup $group): void {
                 $group->id = 'content';
                 $group->weight = 30;
-                $group->addItem(__('Menus'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.menus.sidebar.label', 'Menus')), function (SidebarItem $item): void {
                     $item->id = 'menus';
                     $item->icon = config('typicms.modules.menus.sidebar.icon');
                     $item->weight = config('typicms.modules.menus.sidebar.weight');
@@ -49,10 +49,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read blocks')) {
-            $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.blocks.sidebar.group', 'Content')), function (SidebarGroup $group): void {
                 $group->id = 'content';
                 $group->weight = 30;
-                $group->addItem(__('Content blocks'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.blocks.sidebar.label', 'Content blocks')), function (SidebarItem $item): void {
                     $item->id = 'blocks';
                     $item->icon = config('typicms.modules.blocks.sidebar.icon');
                     $item->weight = config('typicms.modules.blocks.sidebar.weight');
@@ -61,10 +61,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read tags')) {
-            $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.tags.sidebar.group', 'Content')), function (SidebarGroup $group): void {
                 $group->id = 'content';
                 $group->weight = 30;
-                $group->addItem(__('Tags'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.tags.sidebar.label', 'Tags')), function (SidebarItem $item): void {
                     $item->id = 'tags';
                     $item->icon = config('typicms.modules.tags.sidebar.icon');
                     $item->weight = config('typicms.modules.tags.sidebar.weight');
@@ -73,10 +73,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read taxonomies')) {
-            $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.taxonomies.sidebar.group', 'Content')), function (SidebarGroup $group): void {
                 $group->id = 'content';
                 $group->weight = 30;
-                $group->addItem(__('Taxonomies'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.taxonomies.sidebar.label', 'Taxonomies')), function (SidebarItem $item): void {
                     $item->id = 'taxonomies';
                     $item->icon = config('typicms.modules.taxonomies.sidebar.icon');
                     $item->weight = config('typicms.modules.taxonomies.sidebar.weight');
@@ -85,10 +85,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read translations')) {
-            $view->offsetGet('sidebar')->group(__('Content'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.translations.sidebar.group', 'Content')), function (SidebarGroup $group): void {
                 $group->id = 'content';
                 $group->weight = 30;
-                $group->addItem(__('Translations'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.translations.sidebar.label', 'Translations')), function (SidebarItem $item): void {
                     $item->id = 'translations';
                     $item->icon = config('typicms.modules.translations.sidebar.icon');
                     $item->weight = config('typicms.modules.translations.sidebar.weight');
@@ -97,10 +97,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read users')) {
-            $view->offsetGet('sidebar')->group(__('Users and roles'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.users.sidebar.group', 'Users and roles')), function (SidebarGroup $group): void {
                 $group->id = 'users';
                 $group->weight = 50;
-                $group->addItem(__('Users'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.users.sidebar.label', 'Users')), function (SidebarItem $item): void {
                     $item->id = 'users';
                     $item->icon = config('typicms.modules.users.sidebar.icon');
                     $item->weight = config('typicms.modules.users.sidebar.weight');
@@ -109,10 +109,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read roles')) {
-            $view->offsetGet('sidebar')->group(__('Users and roles'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.roles.sidebar.group', 'Users and roles')), function (SidebarGroup $group): void {
                 $group->id = 'users';
                 $group->weight = 50;
-                $group->addItem(__('Roles'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.roles.sidebar.label', 'Roles')), function (SidebarItem $item): void {
                     $item->id = 'roles';
                     $item->icon = config('typicms.modules.roles.sidebar.icon');
                     $item->weight = config('typicms.modules.roles.sidebar.weight');
@@ -121,10 +121,10 @@ class SidebarViewComposer
             });
         }
         if (Gate::allows('read files')) {
-            $view->offsetGet('sidebar')->group(__('Media'), function (SidebarGroup $group): void {
+            $view->offsetGet('sidebar')->group(__(config('typicms.modules.files.sidebar.group', 'Media')), function (SidebarGroup $group): void {
                 $group->id = 'media';
                 $group->weight = 40;
-                $group->addItem(__('Files'), function (SidebarItem $item): void {
+                $group->addItem(__(config('typicms.modules.files.sidebar.label', 'Files')), function (SidebarItem $item): void {
                     $item->id = 'files';
                     $item->icon = config('typicms.modules.files.sidebar.icon');
                     $item->weight = config('typicms.modules.files.sidebar.weight');
