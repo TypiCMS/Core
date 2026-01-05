293059f2b6fd765e1818512197917dac3dc2e017
Samuel De Backer <sdebacker@gmail.com>
2026-01-05 12:45:04 +0100

    Update tiptap-editors.blade.php

--- resources/views/core/components/tiptap-editors.blade.php
+++ resources/views/core/components/tiptap-editors.blade.php
@@ -1,5 +1,5 @@
 @props(['model', 'name', 'label' => null])
 
 @foreach (locales() as $locale)
-    <tiptap-editor name="{{ $name }}[{{ $locale }}]" locale="{{ $locale }}" init-content="{{ $model->getTranslation($name, $locale) }}" :label="'{{ $label }}'"></tiptap-editor>
+    <tiptap-editor name="{{ $name }}[{{ $locale }}]" locale="{{ $locale }}" init-content="{{ old("{$name}.{$locale}", $model->getTranslation('body', $locale)) }}" :label="'{{ $label ?? null }}'"></tiptap-editor>
 @endforeach
