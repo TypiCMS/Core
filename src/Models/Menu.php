7db2e0a34689b131dddaa4dd7a76d464410c539b
Samuel De Backer <sdebacker@gmail.com>
2026-02-18 23:17:27 +0100

    Refactor Menu getMenu and menulink handling

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace TypiCMS\Modules\Core\Models;
 
-use Exception;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
@@ -29,7 +27,7 @@ use TypiCMS\Translatable\HasTranslations;
  * @property-read Collection<int, History> $history
  * @property-read int|null $history_count
  * @property-read File|null $image
- * @property-read NestableCollection<int, Menulink> $menulinks
+ * @property NestableCollection<int, Menulink> $menulinks
  * @property-read int|null $menulinks_count
  * @property-read mixed $thumb
  * @property-read mixed $translations
@@ -53,83 +51,66 @@ class Menu extends Base
 
     public function getMenu(string $name): ?self
     {
-        try {
-            $menu = self::query()
-                ->published()
-                ->with([
-                    'menulinks' => function ($query): void {
-                        $query->with([
-                            'page',
-                            'section',
-                            'image',
-                            'parent',
-                        ])->published();
-                    },
-                ])
-                ->where('name', $name)
-                ->first();
+        $menu = self::query()
+            ->published()
+            ->with([
+                'menulinks' => function ($query): void {
+                    $query->with([
+                        'page',
+                        'section',
+                        'image',
+                        'parent',
+                    ])->published();
+                },
+            ])
+            ->where('name', $name)
+            ->first();
 
-            $menu->menulinks = $this->prepare($menu->menulinks)->nest();
-
-            return $menu;
-        } catch (Exception) {
-            Log::info('No menu found with name “' . $name . '”');
+        if (!$menu) {
+            Log::info("No menu named “{$name}” found.");
 
             return null;
         }
+
+        $menu->menulinks = $menu->menulinks->each(function (Menulink $menulink): void {
+            $menulink->items = new Collection();
+            $menulink->href = $this->setHref($menulink);
+            $menulink->class = $this->setClass($menulink);
+        })->nest();
+
+        return $menu;
     }
 
-    public function prepare(?NestableCollection $items = null): NestableCollection
-    {
-        $items->each(function (Model $item, int $key): void {
-            /** @var Menulink $item */
-            $item->items = new Collection();
-            $item->href = $this->setHref($item);
-            $item->class = $this->setClass($item);
-        });
-
-        return $items;
-    }
-
-    /**
-     * Set the href to the menu link’s website or the URL of the related page.
-     */
     public function setHref(Menulink $menulink): string
     {
         if ($menulink->website) {
             return $menulink->website;
         }
 
-        if ($menulink->page) {
-            $url = $menulink->page->path();
-            if ($menulink->section) {
-                return $url . '#' . $menulink->section->slug . '-' . $menulink->section->id;
-            }
-
-            return $url;
+        if (!$menulink->page) {
+            return '/';
         }
 
-        return '/';
+        $url = $menulink->page->path();
+
+        if ($menulink->section) {
+            return "{$url}#{$menulink->section->slug}-{$menulink->section->id}";
+        }
+
+        return $url;
     }
 
-    /**
-     * Set the class and add active if needed.
-     */
     public function setClass(Menulink $menulink): string
     {
-        $classArray = (array) preg_split('/ /', (string) $menulink->class, -1, PREG_SPLIT_NO_EMPTY);
-        // add active class if current uri is equal to item uri or contains
-        // item uri and is bigger than 3 to avoid homepage link always active ('/', '/lg')
-        $pattern = $menulink->href;
-        if (mb_strlen((string) $menulink->href) > 3) {
-            $pattern .= '*';
-        }
+        $classes = collect(explode(' ', (string) $menulink->class))->filter();
+
+        $pattern = mb_strlen((string) $menulink->href) > 3 ? "{$menulink->href}*" : $menulink->href;
 
         if (request()->is($pattern)) {
-            $classArray[] = 'active';
+            $classes->push('active');
         }
 
-        return implode(' ', $classArray);
+        return $classes->implode(' ');
     }
 
     /** @return Attribute<string, null> */
