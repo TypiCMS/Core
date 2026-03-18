8034b5e670cce338746a9e889c1b1e6ff55e113d
Samuel De Backer <sdebacker@gmail.com>
2026-03-18 21:44:08 +0100

    refactor items-navigator

--- resources/views/core/components/items-navigator.blade.php
+++ resources/views/core/components/items-navigator.blade.php
@@ -1,16 +1,20 @@
-@props(['model', 'page', 'lang' => app()->getLocale()])
+@props(['model', 'page'])
 
 <div class="items-navigator">
-    <a class="items-navigator-back" href="{{ $page->url($lang) }}">
+    <a class="items-navigator-back" href="{{ $page->url() }}">
         ←
         @lang("Back to {$model->getTable()} list")
     </a>
     <div class="items-navigator-previous-next">
-        <a class="items-navigator-previous @if (!($prev = (new ($model::class)())->prev($model))) disabled @endif" href="@if ($prev) {{ route($lang . '::' . Str::singular($model->getTable()), $prev->slug) }} @endif">
+        @php
+            $prevUrl = new ($model::class)()->prev($model)?->url();
+            $nextUrl = new ($model::class)()->next($model)?->url();
+        @endphp
+        <a @class(['items-navigator-previous', 'disabled' => !$prevUrl]) href="{{ $prevUrl }}">
             ←
             @lang('Previous')
         </a>
-        <a class="items-navigator-next @if (!($next = (new ($model::class)())->next($model))) disabled @endif" href="@if ($next) {{ route($lang . '::' . Str::singular($model->getTable()), $next->slug) }} @endif">
+        <a @class(['items-navigator-next', 'disabled' => !$nextUrl]) href="{{ $nextUrl }}">
             @lang('Next')
             →
         </a>
