25a88603e2a226c5c16d1acd58a7f0f3ba38dc0b
Samuel De Backer <sdebacker@gmail.com>
2026-03-18 13:38:19 +0100

    Use @class and accessibility updates in menu views

--- resources/views/menus/public/_item.blade.php
+++ resources/views/menus/public/_item.blade.php
@@ -1,13 +1,12 @@
-<li class="{{ $name }}-nav-item {{ $name }}-nav-item-{{ $menulink->id }} {{ $menulink->class }}" id="menuitem_{{ $menulink->id }}" role="menuitem">
-    <a class="{{ $name }}-nav-link {{ $menulink->items->count() > 0 ? 'dropdown-toggle' : '' }}{{ !empty($menulink->parent) ? 'dropdown-item' : '' }}" href="{{ $menulink->items->count() > 0 ? '#' : url($menulink->href) }}" @if ($menulink->target === '_blank') target="_blank"
-        rel="noopener noreferrer" @endif @if ($menulink->items->count() > 0) role="button" id="menuitem_{{ $menulink->id }}_id" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false" @endif>
+<li @class([$name.'-nav-item', $name.'-nav-item-'.$menulink->id, $menulink->class]) role="none">
+    <a @class([$name.'-nav-link', $menulink->class, 'dropdown-toggle' => $menulink->items->count() > 0, 'dropdown-item' => $menulink->parent !== null]) @if (url($menulink->href) === url()->current()) aria-current="page" @endif href="{{ $menulink->items->count() > 0 ? '#' : url($menulink->href) }}" @if ($menulink->target === '_blank') target="_blank" rel="noopener noreferrer" @endif @if ($menulink->items->count() > 0) role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false" @endif>
         @if ($menulink->image !== null)
-            <img class="{{ $name }}-nav-image" aria-hidden="true" src="{{ $menulink->imageUrl(64, 64) }}" width="32" height="32" alt="" />
+            <img @class([$name.'-nav-image']) aria-hidden="true" src="{{ $menulink->imageUrl(64, 64) }}" width="32" height="32" alt="" />
         @endif
-        <span class="{{ $name }}-nav-label">{{ $menulink->title }}</span>
+        <span @class([$name.'-nav-label'])>{{ $menulink->title }}</span>
     </a>
     @if ($menulink->items->count() > 0)
-        <ul class="{{ $name }}-nav-dropdown dropdown-menu" aria-labelledby="menuitem_{{ $menulink->id }}_id" role="menu">
+        <ul @class([$name.'-nav-dropdown', 'dropdown-menu']) role="menu">
             @foreach ($menulink->items as $menulink)
                 @include('menus::public._item', ['menulink' => $menulink])
             @endforeach
--- resources/views/menus/public/_menu.blade.php
+++ resources/views/menus/public/_menu.blade.php
@@ -1,6 +1,6 @@
 @if ($menu = (new TypiCMS\Modules\Core\Models\Menu())->getMenu($name))
     @if ($menulinks = $menu->menulinks->load('image') and $menulinks->count() > 0)
-        <ul class="{{ $name }}-nav-list {{ $menu->class }}" role="menu">
+        <ul @class([$name.'-nav-list', $menu->class]) role="menubar">
             @foreach ($menulinks as $menulink)
                 @include('menus::public._item')
             @endforeach
