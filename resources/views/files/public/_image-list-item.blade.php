069e9ba42fcc8ac88937e55ff3c08c65cbd3bc52
Samuel De Backer <134503+sdebacker@users.noreply.github.com>
2026-03-25 09:32:44 +0100

    Update _image-list-item.blade.php

--- resources/views/files/public/_image-list-item.blade.php
+++ resources/views/files/public/_image-list-item.blade.php
@@ -1,6 +1,6 @@
 <li class="image-list-item">
-    <a class="image-list-item-link lightbox-item" href="{{ $image?->render(1600, 1600, ['resize']) }}" data-pswp-width="{{ $image->width }}" data-pswp-height="{{ $image->height }}">
-        <img class="image-list-item-image" src="{{ $image?->render(null, 300) }}" width="{{ $image->width }}" height="{{ $image->height }}" alt="@lang('Click to enlarge')" />
+    <a class="image-list-item-link lightbox-item" href="{{ $image->render(1600, 1600, ['resize']) }}" data-pswp-width="{{ $image->width }}" data-pswp-height="{{ $image->height }}">
+        <img class="image-list-item-image" src="{{ $image->render(null, 300) }}" width="{{ $image->width }}" height="{{ $image->height }}" alt="@lang('Click to enlarge')" />
         @if ($image->description)
             <div class="hidden-caption-content">{{ $image->description }}</div>
         @endif
